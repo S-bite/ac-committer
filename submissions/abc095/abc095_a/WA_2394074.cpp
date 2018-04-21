84aff2a6654c42782024c5b904ca9fd740f46d62
S-bite <0125bitbite@gmail.com>
2018-04-21 21:50:29 +0900

    "[WA] abc095_a"

---
+++ submissions/abc095/abc095_a/WA_2394074.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int price=600;
+  string s;
+  cin>>s;
+  for (int i=0;i<3;i++){
+    if( char(s[i])==char('o')) price+=100;
+  }
+  cout<<price<<endl;
+  return 0;
+}
