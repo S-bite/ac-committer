3aa0e4ce635febed1406c3c5f183cc1d8851bce9
S-bite <0125bitbite@gmail.com>
2018-06-16 22:25:00 +0900

    "[CE] abc100_b"

---
+++ submissions/abc100/abc100_b/CE_2687813.c
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int D,N;
+  cin>>D>>N;
+  if (D==0) cout<<N<<endl;
+  if (D==1) cout<<100*N<<endl;
+  if (D==2) cout<<10000*N<<endl;
+}
