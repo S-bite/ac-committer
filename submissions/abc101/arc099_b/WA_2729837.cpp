7deb8797ed4084eff846e7bb71d0a8b78ad3c0df
S-bite <0125bitbite@gmail.com>
2018-06-23 22:11:02 +0900

    "[WA] arc099_b"

---
+++ submissions/abc101/arc099_b/WA_2729837.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int K;
+  cin>>K;
+  if (K<10){
+    for (int i=0;i<K;i++){
+      cout<<i+1<<endl;
+    }
+
+  }
+  else{
+    for (int i=0;i<9;i++){
+      cout<<i+1<<endl;
+    }
+      K-=9;
+      for (int i=0;i<K;i++){
+        cout<<(i+1)*10+9<<endl;
+      }
+
+  }
+}
