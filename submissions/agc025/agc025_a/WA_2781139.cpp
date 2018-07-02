83f968e4eb557148d397f8706d0d24d707e6d433
S-bite <0125bitbite@gmail.com>
2018-07-02 13:47:38 +0900

    "[WA] agc025_a"

---
+++ submissions/agc025/agc025_a/WA_2781139.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int f(int n){
+  int res=0;
+  while(n){
+    res+=n%10;
+    n/=10;
+  }
+  return res;
+}
+
+int main(){
+  int N;
+  int ans=65536;
+
+  cin>>N;
+  for (int i=1;i<N/2;i++){
+    if (ans>(f(i)+f(N-i))){
+      ans=(f(i)+f(N-i));
+      cout<<i<<","<<N-i<<endl;
+    }
+  }
+}
