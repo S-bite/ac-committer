a85f49f02ef6f924c2985c835f5cbf2d243514ee
S-bite <0125bitbite@gmail.com>
2018-07-06 14:12:45 +0900

    "[WA] abc015_4"

---
+++ submissions/abc015/abc015_4/WA_2795957.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int W,N,K;
+    cin>>W>>N>>K;
+    int A[N],B[N];
+    for (int i=0;i<N;i++) cin>>A[i]>>B[i];
+    vector< vector< vector<int> > > dp(N+1,vector<vector<int> >(W+1,vector<int>(K+1,0)));
+    for(int i=1;i<=N;i++){
+        for(int j=0;j<=W;j++){
+            for(int k=1;k<=K;k++){
+                if ((j-A[i-1])<0) continue;
+                if (dp[i-1][j-A[i-1]][k-1]+B[i-1]>dp[i-1][j][k])dp[i][j][k]=dp[i-1][j-A[i-1]][k-1]+B[i-1];
+                else dp[i][j][k]=dp[i-1][j][k];
+                //if (A[i]+j>W)continue;
+
+                //dp[i+1][j+A[i]][k+1]=max(dp[i][j][k]+B[i],dp[i+1][j+A[i]][k+1]);
+                //dp[i+1][j][k]=max(dp[i][j][k],dp[i+1][j][k]);
+
+              //  if(dp[i][j][k]+B[i]>dp[i+1][j+A[i]][k+1]){
+                //  cout<<i<<","<<j<<","<<k<<","<<dp[i][j][k]<<","<<A[i-1]<<","<<B[i-1]<<endl;
+                //}
+            }
+        }}
+
+    int ans=0;
+    for(int i=0;i<=N;i++){
+        for(int j=0;j<W+1;j++){
+            for(int k=0;k<K+1;k++){
+              ans=max(ans,dp[i][j][k]);
+            //  cout<<i<<","<<j<<","<<k<<","<<dp[i][j][k]<<endl;
+            }
+        }
+    }
+    cout<<ans<<endl;
+  }
+
+  /*
+
+  10
+  5 4
+  9 10
+  3 7
+  3 1
+  2 6
+  4 5
+  */
