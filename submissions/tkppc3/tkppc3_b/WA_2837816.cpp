68589eec24c6d0032389687b58200ed0522c808b
S-bite <0125bitbite@gmail.com>
2018-07-14 13:24:35 +0900

    "[WA] tkppc3_b"

---
+++ submissions/tkppc3/tkppc3_b/WA_2837816.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  unsigned long long  i;
+  cin>>i;
+  if(i%6==0)cout<<"yES"<<endl;else cout<<"nO"<<endl;
+  if(i%11==0)cout<<"yES"<<endl;else cout<<"nO"<<endl;
+}
