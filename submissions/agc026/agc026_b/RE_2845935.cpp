7c3886b802a87aa9cab9c4886253a7df0970c3c5
S-bite <0125bitbite@gmail.com>
2018-07-14 22:22:40 +0900

    "[RE] agc026_b"

---
+++ submissions/agc026/agc026_b/RE_2845935.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int T;
+  cin>>T;
+  long long int A,B,C,D;
+  for(int i=0;i<T;i++){
+    cin>>A>>B>>C>>D;
+    long long int a=B-C; //x<C+aにできたら勝ち
+    long long int d=D%B;
+    long long int g=(A-C)%B;
+    if(g==0)g=B;
+    if(B>D){cout<<"No"<<endl;continue;}
+    if(A<B){cout<<"No"<<endl;continue;}
+    if(B<=D and C>=B){cout<<"Yes"<<endl;continue;}
+
+    vector<bool> b(B+1,false);
+  while(true){
+    //cout<<i<<","<<a<<","<<g<<endl;
+    if(g<a){cout<<"No"<<endl;break;}
+    if(b[g]){cout<<"Yes"<<endl;break;}
+    b[g]=true;
+    g=(g-B+D)%B;
+    if(g==0)g=B;
+  }
+  }
+}
+/*14 10 7 11
+15
+16
+17
+7*/
+/*
+c=A;
+while(c>0){
+c-=B;
+if(c<=C)c+=D;
+}*/
