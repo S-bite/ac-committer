92b123b98f69e6f6456de43c946817d44acd2d76
S-bite <0125bitbite@gmail.com>
2018-07-22 00:00:22 +0900

    "[RE] soundhound2018_summer_qual_a"

---
+++ submissions/soundhound2018-summer-qual/soundhound2018_summer_qual_a/RE_2888258.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define INF 20000000001
+using namespace std;
+struct edge{
+  int to,cost_y,cost_s;
+};
+
+int main(){
+  long long int n,m,s,t;
+  cin>>n>>m>>s>>t;
+  long long int d_s[n][n]={INF};
+  cout<<"t"<<endl;
+  vector<edge> edges[n];
+  for(int i=0;i<n;i++){
+    edge e={i,0,0};
+    edges[i].push_back(e);
+  }
+
+
+  int u[m],v[m],a[m],b[m];
+
+  for(int i=0;i<m;i++){
+    cin>>u[i]>>v[i]>>a[i]>>b[i];
+    edge e1={u[i]-1,a[i],b[i]};
+    edges[v[i]-1].push_back(e1);
+    edge e2={v[i]-1,a[i],b[i]};
+    edges[u[i]-1].push_back(e2);
+}
+
+}
