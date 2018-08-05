f7ee4b60d030690cc9b7d3113fc675ada0c69054
S-bite <0125bitbite@gmail.com>
2018-08-05 22:21:38 +0900

    "[WA] abc104_c"

---
+++ submissions/abc104/abc104_c/WA_2957997.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int D,G;
+  cin>>D>>G;
+  int p[D],c[D];
+  for(int i=0;i<D;i++)cin>>p[i]>>c[i];
+  pair<int, pair <int,int > > a[D];
+  for(int i=0;i<D;i++){
+    a[i].first=(((i+1)*100*p[i]+c[i])/p[i]);
+    a[i].second.first=(i+1)*100*p[i]+c[i];
+    a[i].second.second=i;
+  }
+  bool used[D]={false};
+  sort(a,a+D,greater<pair<int, pair <int,int> > >());
+  int ans=0;
+  for(int i=0;i<D;i++){
+    if (G>a[i].second.first){G-=a[i].second.first;ans+=p[a[i].second.second];used[a[i].second.second]=true;}
+    else break;
+  }
+  int _min=999;
+  for(int i =0;i<D;i++){
+    if (used[i]) continue;
+    if(G<=(1+i)*100*(p[i]-1)) _min=min(_min,(G+(1+i)*100-1)/((1+i)*100));
+    else if(G>(i+1)*100*(p[i]-1) and G<=(i+1)*100*p[i]+c[i]) _min=min(_min,p[i]);
+  }
+  cout<<_min+ans<<endl;
+}
