cbc1c1bceaf272955d31665518701f88055d8361
S-bite <0125bitbite@gmail.com>
2018-08-21 07:19:21 +0900

    "[CE] arc062_a"

---
+++ submissions/abc046/arc062_a/CE_3051180.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int N;
+  cin>>N;
+  unsigned long long A,B;
+  cin>>A>>B;
+  for(int i=1;i<N;i++){
+    //cout<<A<<","<<B<<endl;
+
+  unsigned  long long t,s;
+    cin>>t>>s;
+    long long c=max((A+t-1)/t,(B+s-1)/s);
+    A=t*max(c,1);
+    B=s*max(c,1);
+  }
+  cout<<(unsigned long long)(A+B)<<endl;
+  //cout<<(unsigned long long)1000000000000000000<<endl;
+  return 0;
+}
