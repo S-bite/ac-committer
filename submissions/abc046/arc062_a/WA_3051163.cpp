cb652d0c019d554eb91cfc3f1a4e652134938e68
S-bite <0125bitbite@gmail.com>
2018-08-21 07:02:36 +0900

    "[WA] arc062_a"

---
+++ submissions/abc046/arc062_a/WA_3051163.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int N;
+  cin>>N;
+  long long A,B;
+  cin>>A>>B;
+  for(int i=1;i<N;i++){
+    //cout<<A<<","<<B<<endl;
+
+    long long t,s;
+    cin>>t>>s;
+    int c=max((A+t-1)/t,(B+s-1)/s);
+    A=t*max(c,1);
+    B=s*max(c,1);
+  }
+  cout<<(A+B)<<endl;
+  return 0;
+}
