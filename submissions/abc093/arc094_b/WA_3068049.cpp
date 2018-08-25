26305093087fd92b0335cd643d1a0c3db833d645
S-bite <0125bitbite@gmail.com>
2018-08-25 09:36:33 +0900

    "[WA] arc094_b"

---
+++ submissions/abc093/arc094_b/WA_3068049.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int doublling(long long x,long long y){
+  int ret=0;
+  long long t=x*y-1;
+  if ((x==1) && (floor(t/(x))!=floor(t/(x+1)))) ret++;
+  else if (floor(t/(x))!=floor(t/(x-1)) && (floor(t/(x))!=floor(t/(x+1)))) ret++;
+//  cout<<"x:"<<x<<",y:"<<y<<",ret:"<<ret<<endl;
+  return ret;
+}
+int check(long long x,long long y){
+  if ( floor((x*y-1)/floor(sqrt((x*y-1))))==floor(sqrt((x*y-1)))) return 1;
+  else return 0;
+}
+int main(){
+  int Q;
+  cin>>Q;
+  for (int i=0;i<Q;i++){
+    long long x,y;
+    cin>>x>>y;
+  //  cout<<(isusebothside(x,y)?"t":"f")<<endl;
+    cout<<setprecision(12)<<2*(floor(sqrt(x*y-1)))-check(x,y)-doublling(x,y)-doublling(y,x)<<endl;
+  }
+  return 0;
+}
