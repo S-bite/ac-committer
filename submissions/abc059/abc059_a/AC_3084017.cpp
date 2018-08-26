e566340dab6f3bc4b68dc83bb88bc85721d4a716
S-bite <0125bitbite@gmail.com>
2018-08-26 16:21:15 +0900

    "[AC] abc059_a"

---
+++ submissions/abc059/abc059_a/AC_3084017.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string a,b,c;
+    cin>>a>>b>>c;
+	cout<<(char)((int)a[0]-'a'+'A')<<(char)((int)b[0]-'a'+'A')<<(char)((int)c[0]-'a'+'A')<<endl;
+  	return 0;
+}
