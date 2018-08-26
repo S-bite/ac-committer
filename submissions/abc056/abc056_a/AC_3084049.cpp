66a0d787217da8423f8fd00e036e479ae8fa067c
S-bite <0125bitbite@gmail.com>
2018-08-26 16:27:19 +0900

    "[AC] abc056_a"

---
+++ submissions/abc056/abc056_a/AC_3084049.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	char a,b;
+  cin>>a>>b;
+	if (a=='H' and b=='H') cout<<"H"<<endl;
+	if (a=='H' and b=='D') cout<<"D"<<endl;
+	if (a=='D' and b=='H') cout<<"D"<<endl;
+  	if (a=='D' and b=='D') cout<<"H"<<endl;
+
+  return 0;
+}
