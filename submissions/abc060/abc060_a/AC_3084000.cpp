af7246d89dbb8da79683c5614642b7120af41fa8
S-bite <0125bitbite@gmail.com>
2018-08-26 16:18:19 +0900

    "[AC] abc060_a"

---
+++ submissions/abc060/abc060_a/AC_3084000.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string a,b,c;
+  cin>>a>>b>>c;
+	if(a[a.size()-1]==b[0] and b[b.size()-1]==c[0]) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+  return 0;
+}
