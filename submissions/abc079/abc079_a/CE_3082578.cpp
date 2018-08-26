a729b11c13a776f301298fd675a64bfaf988cb5a
S-bite <0125bitbite@gmail.com>
2018-08-26 10:38:57 +0900

    "[CE] abc079_a"

---
+++ submissions/abc079/abc079_a/CE_3082578.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  sting s;
+  cin>>s;
+  if((s[0]==s[1] and s[1]==s[2])or (s[1]==s[2] and s[2]==s[3])) cout<<"Yes"<<endl;
+  else cout<<"No"<<endl;
+  return 0;
+}
