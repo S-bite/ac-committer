36db214556d52147b33ad02809bc267eb71081c9
S-bite <0125bitbite@gmail.com>
2018-08-27 22:31:06 +0900

    "[AC] abc006_1"

---
+++ submissions/abc006/abc006_1/AC_3090117.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+int main(){
+	int a;
+  cin>>a;
+	if(a%3==0) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+  return 0;
+}
