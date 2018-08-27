5ae9c252360d3e4cf482aad45db704d70df0c52a
S-bite <0125bitbite@gmail.com>
2018-08-27 21:24:18 +0900

    "[AC] abc021_a"

---
+++ submissions/abc021/abc021_a/AC_3089748.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+int main(){
+  int n;
+  cin>>n;
+  cout<<n<<endl;
+	rep(i,n) cout<<1<<endl;
+  return 0;
+}
