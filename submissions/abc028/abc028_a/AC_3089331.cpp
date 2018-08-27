a4c43fb6a70f99a0145f0500390b9d631270fcc1
S-bite <0125bitbite@gmail.com>
2018-08-27 19:31:32 +0900

    "[AC] abc028_a"

---
+++ submissions/abc028/abc028_a/AC_3089331.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+  	if (a<60) cout<<"Bad"<<endl;
+  else if (a<90) cout<<"Good"<<endl;
+  else if (a<100)cout<<"Great"<<endl;
+  else cout<<"Perfect"<<endl;
+  return 0;
+}
