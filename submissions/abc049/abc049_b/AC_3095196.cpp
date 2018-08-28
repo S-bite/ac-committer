504c8ffa3906fd35da5592958340d92ecb7bfbd4
S-bite <0125bitbite@gmail.com>
2018-08-29 06:27:25 +0900

    "[AC] abc049_b"

---
+++ submissions/abc049/abc049_b/AC_3095196.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+int main(){
+	int h,w;
+	cin>>h>>w;
+	rep(i,h){
+		string s;
+		cin>>s;
+		cout<<s<<endl;
+		cout<<s<<endl;
+	}
+	return 0;
+}
