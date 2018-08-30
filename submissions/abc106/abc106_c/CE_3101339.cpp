0c374ec036f2f9285af8bd5391ab95ef25d23b9f
S-bite <0125bitbite@gmail.com>
2018-08-30 12:12:37 +0900

    "[CE] abc106_c"

---
+++ submissions/abc106/abc106_c/CE_3101339.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+using namespace std;
+int main(){
+  	string s;
+  	int k;
+  	cin>>s>>k;
+  	rep(i,min(s.size(),k)) if (s[i]!='1') {cout<<s[i]<<endl;return 0;}
+	cout<<"1"<<endl;	
+  return 0;
+}
