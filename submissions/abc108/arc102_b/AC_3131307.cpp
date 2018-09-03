c301454067e7d2fd12b231a93a08db0c87f506e4
S-bite <0125bitbite@gmail.com>
2018-09-03 22:49:59 +0900

    "[AC] arc102_b"

---
+++ submissions/abc108/arc102_b/AC_3131307.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+using namespace std;
+
+int main(){
+  long long l;
+  cin>>l;
+  int n=floor(log2(l));
+  //long long dp[n]={};
+  string ans="";
+  int m=0;
+  rep(i,n){
+        ans+=to_string(i+1)+" "+to_string(i+2)+" 0"+"\n";
+        m++;
+        ans+=to_string(i+1)+" "+to_string(i+2)+" "+to_string(1<<(n-i-1))+"\n";
+        m++;
+  }
+
+  long long cmax=(1<<n);
+  l-=cmax;
+  int cn;
+  while (l>=1){
+  cn=n-floor(log2(l))+1;
+  ans+="1 "+to_string(cn)+" "+to_string(cmax)+"\n";
+  m++;
+  cmax+=(1<<(int)floor(log2(l)));
+  l-=(1<<(int)floor(log2(l)));
+
+}
+  cout<<n+1<<" "<<m<<endl<<ans;
+  return 0;
+}
