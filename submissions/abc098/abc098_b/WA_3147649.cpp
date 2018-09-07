40491e31fbd7f928b6846e0220531afbed40779a
S-bite <0125bitbite@gmail.com>
2018-09-07 23:21:54 +0900

    "[WA] abc098_b"

---
+++ submissions/abc098/abc098_b/WA_3147649.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+using namespace std;
+int f(int a[26],int b[26]){
+  int ret=0;
+  rep(i,26) if(a[i]!=0 and b[i]!=0) ret++;
+  return ret;
+}
+int main(){
+  string s;
+  cin>>s;
+  int h[26]={},t[26]={};
+  rep(i,s.size()) t[(int)s[i]-'a']++;
+  int ans=0;
+  rep(i,s.size()){
+    t[(int)s[i]-'a']--;
+    h[(int)s[i]-'a']++;
+    ans=max(ans,f(h,t));
+  }
+  cout<<ans<<endl;
+  return 0;
+}
