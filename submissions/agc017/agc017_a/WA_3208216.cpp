d97dd3745811e75953f07cfc7a92c6da9ed643c1
S-bite <0125bitbite@gmail.com>
2018-09-16 10:11:55 +0900

    "[WA] agc017_a"

---
+++ submissions/agc017/agc017_a/WA_3208216.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define ll long long
+using namespace std;
+
+int main(){
+  int n,f,even=0,odd=0;
+  cin>>n>>f;
+  rep(i,n){
+    int t;
+    cin>>t;
+    if (t%2==0) even++;else odd++;
+  }
+  if (odd==0) {
+    if (f==0) cout<<1<<endl;
+    else cout<<0<<endl;
+  }else{
+      cout<<setprecision(64)<<(ll)pow(2,n-1)<<endl;
+  }
+  return 0;
+}
