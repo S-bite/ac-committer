097885623466e6ffd3953754bf3e72540670a511
S-bite <0125bitbite@gmail.com>
2018-09-18 08:06:33 +0900

    "[WA] arc075_a"

---
+++ submissions/abc063/arc075_a/WA_3218418.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define ll long long
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int a[n],sum=0;
+  rep(i,n){
+    int t;
+    cin>>t;
+    sum+=t;
+    a[i]=t;
+  }
+  sort(a,a+n);
+  rep(i,n) {
+    if (sum%10!=0){
+      cout<<sum<<endl;
+      return 0;
+    }
+    sum-=a[i];
+  }
+  cout<<sum<<endl;
+  return 0;
+}
