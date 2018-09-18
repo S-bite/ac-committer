88184f4afb458ce0c3e58baff7c43314a09b60c5
S-bite <0125bitbite@gmail.com>
2018-09-18 09:22:29 +0900

    "[WA] arc075_b"

---
+++ submissions/abc063/arc075_b/WA_3218543.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define ll long long
+using namespace std;
+bool enough(vector<int>h,int T,int a,int b){
+  int d=a-b;
+  int t=0;
+  rep(i,h.size()){
+    if (h[i]-b*T>0){
+      t+=(h[i]-b*T+d-1)/d;
+    }
+  }
+  return t<T;
+}
+int main(){
+  int n,a,b;
+  cin>>n>>a>>b;
+  vector<int>h;
+  rep(i,n){
+    int t;
+    cin>>t;
+    h.push_back(t);
+  }
+  int upper=(1<<30)/b+1,lower=0;
+  while (upper!=lower+1){
+  //  cout<<upper<<","<<lower<<endl;
+    int T=(upper+lower)/2;
+    if(enough(h,T,a,b)){
+      upper = T;
+    }else lower=T;
+  }
+  cout<<upper<<endl;
+}
