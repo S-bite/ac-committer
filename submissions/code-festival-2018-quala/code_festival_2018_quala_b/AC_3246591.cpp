54b12e562efce9f0be1a8dc39d849a8e94108b95
S-bite <0125bitbite@gmail.com>
2018-09-23 07:50:06 +0900

    "[AC] code_festival_2018_quala_b"

---
+++ submissions/code-festival-2018-quala/code_festival_2018_quala_b/AC_3246591.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define ll long long
+using namespace std;
+
+
+int main(){
+  int n,m,a,b;
+  cin>>n>>m>>a>>b;
+  int l[m],r[m];
+  bool chk[n];
+  rep(i,n) chk[i]=false;
+  rep(i,m){
+    cin>>l[i]>>r[i];
+  }
+
+  rep(i,m){
+    for(int j=l[i];j<=r[i];j++)
+    {
+chk[j-1]=true;
+    //  cout<<j<<endl;
+    }
+  }
+  int ans=0;
+  rep(i,n) {
+    if (chk[i]) ans+=a;else ans+=b;
+  }
+  cout<<ans<<endl;
+
+  return 0;
+}
