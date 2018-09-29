a83ce758a95f3005b5b277180932f9108ff952e4
S-bite <0125bitbite@gmail.com>
2018-09-29 21:19:42 +0900

    "[RE] arc103_a"

---
+++ submissions/arc103/arc103_a/RE_3293524.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for (ll i=0;i<(ll)(n);i++)
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  pair<int,int> a[10020],b[10020];
+  rep(i,10020){
+    a[i].first=0;
+    a[i].second=i;
+
+    b[i].first=0;
+    b[i].second=i;
+  }
+  rep(i,n){
+    int t;
+    cin>>t;
+    if(i%2==0){
+      a[t].first++;
+    }else{
+      b[t].first++;
+    }
+  }
+  sort(a,a+10020,greater<pair<int,int> >());
+  sort(b,b+10020,greater<pair<int,int> >());
+  //cout<<a[0].second<<","<<b[0].second<<endl;
+  if (a[0].second==b[0].second){
+    cout<<min((n-a[0].first-b[1].first),(n-a[1].first-b[0].first))<<endl;;
+  }else{
+      cout<<(n-a[0].first-b[0].first)<<endl;
+  }
+  return 0;
+}
