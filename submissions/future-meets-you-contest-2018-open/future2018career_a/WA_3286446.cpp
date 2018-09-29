6e81f3238c3a1374469cb72573b0f780ec9850d9
S-bite <0125bitbite@gmail.com>
2018-09-29 14:03:56 +0900

    "[WA] future2018career_a"

---
+++ submissions/future-meets-you-contest-2018-open/future2018career_a/WA_3286446.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for (ll i=0;i<(ll)(n);i++)
+using namespace std;
+int main(){
+  int n,k;
+  cin>>n>>k;
+  rep(i,k)cout<<0<<" "<<1<<" "<<2<<" "<<3<<" "<<0<<endl;
+  return 0;
+}
