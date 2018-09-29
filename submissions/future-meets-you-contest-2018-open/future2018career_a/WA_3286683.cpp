5e67982d28d2af869f384d1112826a9b7e2a34b7
S-bite <0125bitbite@gmail.com>
2018-09-29 14:31:46 +0900

    "[WA] future2018career_a"

---
+++ submissions/future-meets-you-contest-2018-open/future2018career_a/WA_3286683.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for (ll i=0;i<(ll)(n);i++)
+#define fir first
+#define sec second
+using namespace std;
+void p(int i, int j,int k,int l,int v){
+  cout<<i<<" "<<j<<" "<<k<<" "<<l<<" "<<v<<endl;
+  return ;
+}
+
+int main(){
+  string NOOP="1 2 3 4 0";
+  int n,k;
+  cin>>n>>k;
+  int a[n];
+  pair<int,int>b[n];
+  rep(i,n) {
+    cin>>a[i];
+    b[i]=make_pair(i+1-a[i],i+1);
+    }
+  sort(b,b+n);
+  rep(i,k){
+    int high=n-i,low=i;
+    int v=min(abs(b[high].fir),abs(b[low].fir));
+    p(b[high].sec,b[high].sec,b[low].sec,b[low].sec,v);
+  }
+  return 0;
+}
