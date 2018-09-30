89272291dd53c7c5e23556de66cd8cfa1a3ef3ee
S-bite <0125bitbite@gmail.com>
2018-09-30 21:55:40 +0900

    "[AC] abc111_a"

---
+++ submissions/abc111/abc111_a/AC_3311835.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for(ll i=0;i<(ll)(n);i++)
+using namespace std;
+int main(){
+  string s;
+  cin>>s;
+	rep(i,3)cout<<((s[i]=='1')?"9":"1");
+  cout<<endl;
+}
