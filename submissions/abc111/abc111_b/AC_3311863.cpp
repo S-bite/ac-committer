e8295c3920037c9eb851d5baa60b850000bec18a
S-bite <0125bitbite@gmail.com>
2018-09-30 21:58:57 +0900

    "[AC] abc111_b"

---
+++ submissions/abc111/abc111_b/AC_3311863.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for(ll i=0;i<(ll)(n);i++)
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int ans=111;
+  while(n>ans) ans+=111;
+  cout<<ans<<endl;
+}
