5c0db9825afb5c5aa9d67ea9ea12ffd7c8f1c30f
S-bite <0125bitbite@gmail.com>
2018-10-06 21:01:49 +0900

    "[AC] abc112_a"

---
+++ submissions/abc112/abc112_a/AC_3341800.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for (ll i=0;i<(ll)(n);i++)
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if (n==1) {
+      cout<<"Hello World"<<endl;
+      return 0;
+    }else{
+      int a,b;
+      cin>>a>>b;
+      cout<<(a+b)<<endl;
+      return 0;
+    }
+}
