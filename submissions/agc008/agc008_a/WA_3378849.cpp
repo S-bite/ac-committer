8d98a52c548792d51130546c71e38b4468e375f4
S-bite <0125bitbite@gmail.com>
2018-10-10 21:53:31 +0900

    "[WA] agc008_a"

---
+++ submissions/agc008/agc008_a/WA_3378849.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+  int x,y;
+  cin>>x>>y;
+  int ans;
+  if (x>=0 and y>=0){
+    if(y>x) ans=(y-x);
+    else ans=(x-y+2);
+  }
+  if (x<=0 and y>=0) ans=abs(abs(x)-abs(y))+1;
+  if (x<=0 and y<=0) {
+    if(y<x) ans=abs(y)-abs(x);
+    else ans=abs(y)-abs(x)+2;
+  }
+  if(x>=0 and y<=0)ans=abs(abs(x)-abs(y))+1;
+  cout<<ans<<endl;
+  return 0;
+}
