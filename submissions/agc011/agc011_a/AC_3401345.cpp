65b16cb01c89275551742b3449b65f44459c99bb
S-bite <0125bitbite@gmail.com>
2018-10-14 00:59:35 +0900

    "[AC] agc011_a"

---
+++ submissions/agc011/agc011_a/AC_3401345.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for(long long i=0;i<(ll)(n);i++)
+using namespace std;
+
+int main(){
+  int n,c,k;
+  cin>>n>>c>>k;
+  int t[n];
+  rep(i,n)cin>>t[i];
+  sort(t,t+n);
+  int ans=1,s=t[0],p=1;
+  for(int i=1;i<n;i++){
+    if(p+1>c or s+k<t[i]){
+      //cout<<i<<"+"<<endl;
+      ans++;
+      s=t[i];
+      p=1;
+    }else p++;
+  }
+  cout<<ans<<endl;
+  return 0;
+}
