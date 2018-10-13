e5bddad5941729df024915f0ad1b1cf5433f0a33
S-bite <0125bitbite@gmail.com>
2018-10-13 22:10:50 +0900

    "[TLE] agc028_a"

---
+++ submissions/agc028/agc028_a/TLE_3397038.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for(long long i=0;i<(ll)(n);i++)
+using namespace std;
+ll gcd(ll a,ll b){
+    if(a<b){
+      ll t=a;
+      a=b;
+      b=t;
+    }
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+ll lcm(ll a,ll b){
+  return ((ll)(a*b))/gcd(a,b);
+}
+int main(){
+  //cout<<lcm(10000,100001)<<endl;
+  ll n,m;
+  cin>>n>>m;
+  string s,t;
+  cin>>s>>t;
+  ll ans=lcm(n,m);
+
+  ll N=ans/n;
+  ll M=ans/m;
+  rep(i,n)rep(j,m){
+    //if (i*N<j*M) break;
+    if (i*N==j*M){
+      //cout<<s[i]<<","<<t[j]<<endl;
+      if (s[i]!=t[j]){
+        cout<<-1<<endl;
+        return 0;
+      }
+    }
+  }
+  cout<<ans<<endl;
+  return 0;
+}
