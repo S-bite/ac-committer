52b994937e637f4e985eb874639c0a9e9248983c
S-bite <0125bitbite@gmail.com>
2018-10-14 21:01:10 +0900

    "[AC] code_festival_2018_qualb_a"

---
+++ submissions/code-festival-2018-qualb/code_festival_2018_qualb_a/AC_3405384.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for(long long i=0;i<(ll)(n);i++)
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  cout<<100-(100/n)<<endl;
+
+  return 0;
+}
