11dba07c62d8babdc59f481d938e3d90d16392bf
S-bite <0125bitbite@gmail.com>
2018-10-21 20:39:15 +0900

    "[AC] qupc2018_a"

---
+++ submissions/qupc2018/qupc2018_a/AC_3446698.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define ll long long
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  cout<<2014+(n-1)*4<<endl;
+  return 0;
+}
