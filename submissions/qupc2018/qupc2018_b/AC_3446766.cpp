93af195996e7c697235bd876e37c3d1eb0ff578d
S-bite <0125bitbite@gmail.com>
2018-10-21 20:49:37 +0900

    "[AC] qupc2018_b"

---
+++ submissions/qupc2018/qupc2018_b/AC_3446766.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define ll long long
+#define int long long
+using namespace std;
+ main(){
+  int q;
+  cin>>q;
+  rep(i,q){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if((a*100+b*10+c)%2==1){
+      cout<<"No"<<endl;
+      continue;
+    }
+    int tapi=(a*100+b*10+c)/2;
+    tapi-=min(100*(tapi/100),a*100);
+    tapi-=min(10*(tapi/10),b*10);
+    tapi-=min(tapi,c);
+    if (tapi==0){
+      cout<<"Yes"<<endl;
+    }else{
+      cout<<"No"<<endl;
+    }
+  }
+  return 0;
+}
