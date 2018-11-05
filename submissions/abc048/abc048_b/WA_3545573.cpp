8d6e4de9cebc24a3ffbc0ec9cf4e21d57346647f
S-bite <0125bitbite@gmail.com>
2018-11-05 12:44:12 +0900

    "[WA] abc048_b"

---
+++ submissions/abc048/abc048_b/WA_3545573.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define MOD (long long)1000000007
+using namespace std;
+
+int main(){
+	long long int a,b,x;
+  	cin>>a>>b>>x;
+  	cout<<(b-a+1)/x<<endl;
+  return 0;
+}
