297d2fded0ba83cf3a0c1329ff1a0e5cef728a90
S-bite <0125bitbite@gmail.com>
2018-11-24 22:10:41 +0900

    "[AC] dwacon5th_prelims_b"

---
+++ submissions/dwacon5th-prelims/dwacon5th_prelims_b/AC_3659786.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define rep(i,n) for(int i=0;i<(n);i++)
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    ll a[n],s[n+1],p[(n*(n+1))/2];
+    s[0]=0;
+    rep(i,n){
+     cin>>a[i];
+     s[i+1]=s[i]+a[i];
+    }
+    ll c=0;
+    for(int i=0;i<n;i++)
+    for(int j=i+1;j<=n;j++){p[c]=s[j]-s[i];c++;}
+    sort(p,p+(n*(n+1))/2,greater<ll>());
+    ll ans=0;
+    for(int i=40;i>=0;i--){
+        ll b=1ll<<i;
+        int r=0;
+        for(ll j=0;j<(n*(n+1))/2;j++){
+            if ((b&p[j])!=0) r++;
+
+        }
+        if (r>=k){
+            ans|=b;
+            for(ll j=0;j<(n*(n+1))/2;j++){
+            if ((b&p[j])==0) p[j]=0;
+        }
+     }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
