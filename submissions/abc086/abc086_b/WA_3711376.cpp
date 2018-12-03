e98842f23ad98a57cdf0bd92aab62b9f59f56f89
S-bite <0125bitbite@gmail.com>
2018-12-03 16:54:45 +0900

    "[WA] abc086_b"

---
+++ submissions/abc086/abc086_b/WA_3711376.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool is_pow2(int n){
+    for(int i=0;i<=sqrt(n)+1;i++){
+        if (i*i==n) return true;
+    }   
+    return false;
+}
+
+int main() {
+    string a,b;
+    cin>>a>>b;
+    int n=atoi((a+b).c_str());
+    cout<<n<<endl;
+    if(is_pow2(n))cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+	return 0;
+}
