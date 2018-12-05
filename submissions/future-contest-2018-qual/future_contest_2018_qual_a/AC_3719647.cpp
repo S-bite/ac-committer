5d2a3c11c9f3c024af2d1080be8533fab10b1092
S-bite <0125bitbite@gmail.com>
2018-12-05 11:49:36 +0900

    "[AC] future_contest_2018_qual_a"

---
+++ submissions/future-contest-2018-qual/future_contest_2018_qual_a/AC_3719647.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+int myrand()
+{
+    return rand() % 100;
+}
+int main()
+{
+
+    cout << 1000 << endl;
+    rep(i, 1000)
+    {
+        cout << myrand() << " " << myrand() << " " << (myrand() + 1) << endl;
+    }
+    return 0;
+}
