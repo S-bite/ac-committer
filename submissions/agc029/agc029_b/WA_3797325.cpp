a5ee9d80ef294d46f98f66328dd167502ce56d16
S-bite <0125bitbite@gmail.com>
2018-12-15 21:51:58 +0900

    "[WA] agc029_b"

---
+++ submissions/agc029/agc029_b/WA_3797325.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#define ll long long
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+using namespace std;
+ll targetnum(ll n)
+{
+    ll ret = 1ll << (int)ceil(log2(n));
+    if (ret == n)
+        ret *= 2;
+    return ret;
+}
+
+int main()
+{
+    ll n;
+    cin >> n;
+    ll a[n];
+    map<ll, ll> mp;
+    rep(i, n)
+    {
+        ll t;
+        cin >> t;
+        a[i] = t;
+        mp[t]++;
+    }
+    sort(a, a + n, greater<ll>());
+    ll ans = 0;
+    rep(i, n)
+    {
+        ll t = targetnum(a[i]) - a[i];
+        if (mp[t] > 0)
+        {
+            ans++;
+            mp[t]--;
+            mp[a[i]]--;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
