067b2a0e39af34c16cd64e30315ef0fa9bbf5b14
S-bite <0125bitbite@gmail.com>
2018-12-23 17:58:09 +0900

    "[WA] dwacon5th_final_a"

---
+++ submissions/dwacon5th-final-open/dwacon5th_final_a/WA_3856898.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+int main()
+{
+    ll n, m, k;
+    string s;
+    cin >> n >> m >> k >> s;
+    ll a[m], b[m];
+    bool red[m], blue[m];
+    map<ll, vector<ll>> mp;
+    rep(i, m)
+    {
+        cin >> a[i] >> b[i];
+        a[i]--;
+        b[i]--;
+        mp[a[i]].push_back(b[i]);
+        mp[b[i]].push_back(a[i]);
+    }
+    rep(i, n)
+    {
+        red[i] = false;
+        blue[i] = false;
+    }
+
+    rep(i, m)
+    {
+        if (s[b[i]] == 'R')
+            red[a[i]] = true;
+        else
+            blue[a[i]] = true;
+        if (s[a[i]] == 'R')
+            red[b[i]] = true;
+        else
+            blue[b[i]] = true;
+    }
+    rep(i, n)
+    {
+
+        if (k % 2 == 0)
+        {
+            bool f = true;
+            for (ll j : mp[i])
+            {
+                if (red[j] or !blue[i])
+                {
+                    f = false;
+                    break;
+                }
+            }
+            if (f)
+                cout << "First" << endl;
+            else
+                cout << "Second" << endl;
+        }
+        else
+        {
+            if (blue[i])
+            {
+                cout << "First" << endl;
+            }
+            else
+                cout << "Second" << endl;
+        }
+    }
+
+    return 0;
+}
