74261558fd937a756aaa620c5c80a5c99351b9f9
S-bite <0125bitbite@gmail.com>
2018-12-31 22:54:13 +0900

    "[AC] codefestival_2016_final_b"

---
+++ submissions/cf16-final/codefestival_2016_final_b/AC_3907529.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  int t = 0;
+  rep(i, n)
+  {
+    if ((t + 1) * t / 2 >= n)
+      break;
+    t++;
+  }
+  while (n != 0)
+  {
+    if (t <= n)
+    {
+      cout << t << endl;
+      n -= t;
+    }
+    t--;
+  }
+  return 0;
+}
