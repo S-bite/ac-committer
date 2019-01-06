2822391f811423e68e5ddeedd913bd99f72863fa
S-bite <0125bitbite@gmail.com>
2019-01-06 21:16:14 +0900

    "[AC] dp_b"

---
+++ submissions/dp/dp_b/AC_3942044.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define ll long long
+using namespace std;
+int main(){
+int n,k;
+cin>>n>>k;
+int h[n];
+rep(i,n) cin>>h[i];
+
+ll dp[n];
+dp[0]=0;
+for(int i=1;i<n;i++){
+    dp[i]=1ll<<31;
+    for(int j=i-1;j>=max(i-k,0);j--)dp[i]=min(dp[i],dp[j]+abs(h[i]-h[j]));
+}
+cout<<dp[n-1]<<endl;
+return 0;
+
+}
