9d632341b3ce897b3a18ce07603e3d00b6fd2018
S-bite <0125bitbite@gmail.com>
2019-01-12 22:12:06 +0900

    "[WA] aising2019_c"

---
+++ submissions/aising2019/aising2019_c/WA_3989489.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fir first
+#define sec second
+#define rep(i,n) for(ll i=0;i<(n);i++)
+using namespace std;
+ll h,w;
+void update(string s[],ll x,ll y,ll w,ll h, ll g[][2],ll nowg,bool updated[]){
+    updated[y*w+x]=true;
+    if(s[y][x]=='.') g[nowg][0]++;
+        else g[nowg][1]++;
+    if(x-1>0 and s[y][x-1]!=s[y][x] and updated[y*w+x-1]==false) {
+        update(s,x-1,y,w,h,g,nowg,updated);
+    } 
+    if(x+1<w and s[y][x+1]!=s[y][x] and updated[y*w+x+1]==false) {
+        update(s,x+1,y,w,h,g,nowg,updated);
+    } 
+    if(y-1>0 and s[y-1][x]!=s[y][x] and updated[(y-1)*w+x]==false) {
+        update(s,x,y-1,w,h,g,nowg,updated);
+    } 
+    if(y+1<h and s[y+1][x]!=s[y][x] and updated[(y+1)*w+x]==false) {
+        update(s,x,y+1,w,h,g,nowg,updated);
+    } 
+    return;
+}
+
+int main(){
+    cin>>h>>w;
+    ll g[h*w][2]={};
+
+    bool updated[h*w];
+    rep(i,h*w) updated[i]=false;
+    ll nowg=0;
+    string s[h];
+
+    rep(i,h) cin>>s[i];
+    rep(i,h){
+        rep(j,w){
+            if (updated[i*w+j]==false){
+                update(s,j,i,w,h,g,nowg,updated);
+                nowg++;
+            }
+        }
+    }
+    ll ans=0;
+    rep(i,nowg){
+       ans+=g[i][1]*g[i][0];
+    }
+    cout<<ans<<endl;
+    return 0;
+}
