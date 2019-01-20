090308273764cc61b305a7bb5081132fd34a7e9b
S-bite <0125bitbite@gmail.com>
2019-01-20 21:00:35 +0900

    "[WA] abc116_a"

---
+++ submissions/abc116/abc116_a/WA_4051777.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fir first
+#define sec second
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a, b;
+    cout << a * b / 2 << endl;
+    return 0;
+}
