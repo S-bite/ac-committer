f6c153e88c218cc8a138a7c393cac3d322f0db1a
S-bite <0125bitbite@gmail.com>
2019-01-27 22:25:43 +0900

    "[AC] nikkei2019_qual_d"

---
+++ submissions/nikkei2019-qual/nikkei2019_qual_d/AC_4107591.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fir first
+#define sec second
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define MOD (1000000007ll)
+#define ALL(x) x.begin(), x.end()
+
+using namespace std;
+struct node
+{
+    int id;
+    int root;
+    vector<int> to;
+    vector<int> from;
+};
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    node t[n];
+    rep(i, n)
+    {
+        t[i] = node{i, -1, {}, {}};
+    }
+    rep(i, n + m - 1)
+    {
+        int from, to;
+        cin >> from >> to;
+        from--;
+        to--;
+        t[from].to.push_back(to);
+        t[to].from.push_back(from);
+    }
+    int rootid = 0;
+    while (!t[rootid].from.empty())
+    {
+        rootid = t[rootid].from[0];
+    }
+    //cout << rootid << endl;
+    int nowid = rootid;
+    int nextid;
+    t[rootid].root = -1;
+    queue<int> que;
+    que.push(rootid);
+    while (!que.empty())
+    { //!t[nowid].to.empty())
+        int nowid = que.front();
+        que.pop();
+
+        for (auto ind : t[nowid].to)
+        {
+            if (t[ind].from.size() == 1)
+            {
+                t[ind].root = nowid;
+                que.push(ind);
+            }
+            t[ind].from.erase(remove(ALL(t[ind].from), nowid), t[ind].from.end());
+        }
+    }
+
+    rep(i, n)
+    {
+        cout << t[i].root + 1 << endl;
+    }
+    return 0;
+}
