d401b4636e4e9c79050f45b5fd77da90bfe4e883
S-bite <0125bitbite@gmail.com>
2019-01-29 21:28:24 +0900

    "[AC] abc090_b"

---
+++ submissions/abc090/abc090_b/AC_4122293.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool is_palindromic(int n)
+{
+    int p[5];
+    for (int i = 0; i < 5; i++)
+    {
+        p[i] = n % 10;
+        n /= 10;
+    }
+    return (p[0] == p[4] && p[1] == p[3]);
+}
+int main()
+{
+    int a, b, ans = 0;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++)
+    {
+        if (is_palindromic(i))
+            ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
