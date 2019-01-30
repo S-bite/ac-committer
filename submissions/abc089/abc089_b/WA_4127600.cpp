3320d052a978c7c71645ba8d2deefbcc92766152
S-bite <0125bitbite@gmail.com>
2019-01-30 19:51:44 +0900

    "[WA] abc089_b"

---
+++ submissions/abc089/abc089_b/WA_4127600.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n,ans=0;
+  cin>>n;
+  map<char,bool> mp;
+  for (int i=0;i<n;i++){
+  	char c;
+    cin>>c;
+    if (mp[c]!=true){
+    	ans++;
+      mp[c]=true;
+    }
+  }
+  cout<<ans<<endl;
+return 0;
+}
