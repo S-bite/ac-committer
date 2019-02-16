f3a4449d409a214d6af9af5db6fbe5d0ac0bd5eb
S-bite <0125bitbite@gmail.com>
2019-02-16 21:01:27 +0900

    "[AC] abc118_a"

---
+++ submissions/abc118/abc118_a/AC_4279324.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (b % a == 0)
+    {
+        cout << (a + b) << endl;
+    }
+    else
+        cout << (b - a) << endl;
+    return 0;
+}
