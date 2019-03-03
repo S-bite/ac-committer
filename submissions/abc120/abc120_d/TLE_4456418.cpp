27add1260928d9c3dd58cde64a43e7bdc7f577a8
S-bite <0125bitbite@gmail.com>
2019-03-03 22:11:39 +0900

    "[TLE] abc120_d"

---
+++ submissions/abc120/abc120_d/TLE_4456418.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+struct UF
+{
+    UF *root;
+    ll con;
+};
+UF *root(UF *u)
+{
+    while (u->root != u)
+        u = u->root;
+    return u;
+}
+
+bool isconnected(UF *a, UF *b)
+{
+    return (root(a) == root(b));
+}
+
+void unite(UF *a, UF *b)
+{
+    if (isconnected(a, b))
+        return;
+    a = root(a);
+    b = root(b);
+    b->root = a;
+    a->con += b->con;
+}
+int main()
+{
+    ll n, m;
+    cin >> n >> m;
+    UF u[n];
+    rep(i, n)
+    {
+        u[i] = UF{&u[i], 1};
+    }
+    pair<int, int> p[m];
+    ll rest = n;
+    ll ans[m];
+    ans[m - 1] = n * (n - 1) / 2;
+    //ans[0] = 0;
+    rep(i, m) cin >> p[i].first >> p[i].second;
+    for (int i = m - 2; i >= 0; i--)
+    {
+        int a = p[i + 1].first - 1, b = p[i + 1].second - 1;
+        // cout << a << "," << b << endl;
+
+        if (isconnected(&u[a], &u[b]))
+        {
+            ans[i] = ans[i + 1];
+            continue;
+        }
+        //        cout << root(&u[a])->con << endl;
+        ans[i] = ans[i + 1] - root(&u[a])->con * root(&u[b])->con;
+
+        unite(&u[a], &u[b]);
+
+        // rest -= u[a].con - 1;
+        //cout << "r:" << rest << endl;
+    }
+    rep(i, m) cout << ans[i] << endl;
+    return 0;
+}
