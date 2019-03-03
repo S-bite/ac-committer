021c7f5301d6a13ce506ce5ff74145b5533736df
S-bite <0125bitbite@gmail.com>
2019-03-03 21:03:49 +0900

    "[AC] abc120_a"

---
+++ submissions/abc120/abc120_a/AC_4444251.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << min(b / a, c) << endl;
+    return 0;
+}
