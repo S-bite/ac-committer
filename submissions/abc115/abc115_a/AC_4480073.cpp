e4bf16c9a913bdf00219008ffa181ba59250091c
S-bite <0125bitbite@gmail.com>
2019-03-05 23:01:58 +0900

    "[AC] abc115_a"

---
+++ submissions/abc115/abc115_a/AC_4480073.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    n = 25 - n;
+    cout << "Christmas ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Eve ";
+    }
+    cout << endl;
+    return 0;
+}
