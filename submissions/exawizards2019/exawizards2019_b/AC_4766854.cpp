41062ba89f1ea3bdba1513d94ee6fa893303ea96
S-bite <0125bitbite@gmail.com>
2019-03-30 21:06:52 +0900

    "[AC] exawizards2019_b"

---
+++ submissions/exawizards2019/exawizards2019_b/AC_4766854.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#ifdef DEBUG
+#define line() cout << "[" << __LINE__ << "] ";
+#define dump(i) cout << #i ": " << i << " ";
+#define dumpl(i) cout << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int r = 0, b = 0;
+    rep(i, n)
+    {
+        if (s[i] == 'R')
+            r++;
+        else
+            b++;
+    }
+    if (r > b)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    return 0;
+}
