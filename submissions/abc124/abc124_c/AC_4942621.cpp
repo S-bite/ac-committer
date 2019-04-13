dc207f5f46a0997f1ef68d64d45853845b1c7d52
S-bite <0125bitbite@gmail.com>
2019-04-13 21:06:44 +0900

    "[AC] abc124_c"

---
+++ submissions/abc124/abc124_c/AC_4942621.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string s;
+    cin >> s;
+    int wb = 0, bw = 0;
+    rep(i, s.size())
+    {
+        if ((i % 2 == 0 && s[i] == '0') || (i % 2 == 1 && s[i] == '1'))
+        {
+            wb++;
+        }
+        if ((i % 2 == 1 && s[i] == '0') || (i % 2 == 0 && s[i] == '1'))
+        {
+            bw++;
+        }
+    }
+    cout << min(wb, bw) << endl;
+    return 0;
+}
