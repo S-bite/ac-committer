bfb2accf679d9c1e1e1eab8e1d29857a9cc0802d
S-bite <0125bitbite@gmail.com>
2019-04-14 21:27:42 +0900

    "[WA] s8pc_6_d"

---
+++ submissions/s8pc-6/s8pc_6_d/WA_4975901.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <iomanip>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n != 2)
+    {
+        cout << "This problem is too difficult for me :(" << endl;
+        return 0;
+    }
+    lld x1, x2, r1, r2;
+    cin >> x1 >> r1 >> x2 >> r2;
+    lld r3;
+    if (labs(x1 - x2) >= min(r1, r2))
+        r3 = -1;
+    r3 = powl(powl(max(r1, r2), 3) + powl(min(r1, r2) - labs(x1 - x2), 3), (lld)1.0 / 3.0);
+    cout << setprecision(12) << max({r1, r2, r3}) << endl;
+    return 0;
+}
