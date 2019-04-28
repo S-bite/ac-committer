898eb9e989c96aec82f0676b9aec52a948e34ba2
S-bite <0125bitbite@gmail.com>
2019-04-28 22:52:28 +0900

    "[WA] abc125_d"

---
+++ submissions/abc125/abc125_d/WA_5178138.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+    if (a < b)
+    {
+        int t = a;
+        a = b;
+        b = t;
+    }
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    long long ans = 0;
+    for (int i = 0; i < (n + 1) / 2; i += 2)
+    {
+        if (-(a[i] + a[i + 1]) > 0)
+        {
+            a[i] *= -1;
+            a[i + 1] *= -1;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        ans += a[i];
+    }
+    cout << ans << endl;
+    return 0;
+}
