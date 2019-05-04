0aa3d7673f05c1479eb0c0cb19111aff8529a71a
S-bite <0125bitbite@gmail.com>
2019-05-04 21:16:26 +0900

    "[TLE] agc033_a"

---
+++ submissions/agc033/agc033_a/TLE_5257845.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int h, w;
+    cin >> h >> w;
+    vector<pair<int, int>> v;
+    string g[h];
+    rep(i, h)
+    {
+        string s;
+        cin >> g[i];
+        rep(j, w)
+        {
+            if (g[i][j] == '#')
+            {
+                v.push_back({i, j});
+            }
+        }
+    }
+    ll ans = 0;
+    rep(i, h)
+    {
+        rep(j, w)
+        {
+            if (g[i][j] == '.')
+            {
+                ll dist = 10000;
+
+                rep(k, v.size())
+                {
+                    dist = min(dist, abs(v[k].first - i) + abs(v[k].second - j));
+                    if (dist <= ans)
+                        break;
+                }
+                //   cerr << i << "," << j << "," << dist << endl;
+                ans = max(ans, dist);
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
