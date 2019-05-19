6812da31f3feb4b1510e30ace83aa4fdbe30e1dd
S-bite <0125bitbite@gmail.com>
2019-05-19 22:37:45 +0900

    "[WA] abc126_f"

---
+++ submissions/abc126/abc126_f/WA_5478691.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+int main()
+{
+    ll m, k;
+    cin >> m >> k;
+    if (k == 0)
+    {
+        rep(i, (1ll << m))
+        {
+            cout << i << " " << i << " ";
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+    return 0;
+}
