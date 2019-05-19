1239a4b3ed2a367e0f8cbf7b862de6be00be902f
S-bite <0125bitbite@gmail.com>
2019-05-19 21:02:58 +0900

    "[AC] abc126_a"

---
+++ submissions/abc126/abc126_a/AC_5449043.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <map>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    s[k - 1] = s[k - 1] - 'A' + 'a';
+    cout << s << endl;
+
+    return 0;
+}
