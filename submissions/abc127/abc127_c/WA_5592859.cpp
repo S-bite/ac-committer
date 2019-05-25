7333e35d07a75b8cb2a6989bdd67c4ed0502ed80
S-bite <0125bitbite@gmail.com>
2019-05-25 21:08:05 +0900

    "[WA] abc127_c"

---
+++ submissions/abc127/abc127_c/WA_5592859.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n, m;
+    cin >> n >> m;
+    int mx = n, mn = 1;
+    rep(i, m)
+    {
+        int l, r;
+        cin >> l >> r;
+        mx = min(mx, r);
+        mn = max(mn, l);
+    }
+    cout << mx - mn + 1 << endl;
+    return 0;
+}
