3633f9df801538d9d32ac75e9b6f53e8d1f18bcc
S-bite <0125bitbite@gmail.com>
2019-05-25 14:14:08 +0900

    "[AC] chokudai_S002_d"

---
+++ submissions/chokudai_S002/chokudai_S002_d/AC_5575637.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (int)(a); i < (int)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#ifdef DEBUG
+#define line() cout << "[" << __LINE__ << "] ";
+#define dump(i) cout << #i ": " << i << " ";
+#define dumpl(i) cout << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ll n;
+    cin >> n;
+    ll a[n], b[n];
+  	ll ans=0;
+    rep(i, n) cin >> a[i] >> b[i];
+    rep(i, n)
+    {
+    	ans+=max(a[i],b[i]);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
