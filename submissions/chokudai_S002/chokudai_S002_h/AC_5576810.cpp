e7f3850a44f52ac2c7fb1857bebc7214234abe18
S-bite <0125bitbite@gmail.com>
2019-05-25 14:24:45 +0900

    "[AC] chokudai_S002_h"

---
+++ submissions/chokudai_S002/chokudai_S002_h/AC_5576810.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (int)(a); i < (int)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#ifdef DEBUG
+#define line() cout << "[" << __LINE__ << "] ";
+#define dump(i) cout << #i ": " << i << " ";
+#define dumpl(i) cout << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main(int argc, char const *argv[])
+{
+    ll n;
+    cin >> n;
+    ll a[n], b[n];
+    ll ans = 0;
+    map<pair<int, int>, bool> mp;
+    rep(i, n) cin >> a[i] >> b[i];
+    rep(i, n)
+    {
+        ans = abs(a[i] - b[i]);
+        if (ans == 0)
+            ans = -1;
+        cout << ans << endl;
+    }
+    return 0;
+}
