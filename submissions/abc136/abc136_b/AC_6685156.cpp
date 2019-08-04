43ffcc44b4996740acb0f1bbaec972d81c77c214
S-bite <0125bitbite@gmail.com>
2019-08-04 21:03:43 +0900

    "[AC] abc136_b"

---
+++ submissions/abc136/abc136_b/AC_6685156.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+
+int ordnum(int n)
+{
+    int ret = 0;
+    while (n != 0)
+    {
+        ret++;
+        n /= 10;
+    }
+    return ret;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+    rep(i, n + 1)
+    {
+        if (ordnum(i) % 2 == 1)
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
