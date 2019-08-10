48a5302a07096c227a900ac4478b1486d05d6ea7
S-bite <0125bitbite@gmail.com>
2019-08-10 22:52:25 +0900

    "[RE] abc137_d"

---
+++ submissions/abc137/abc137_d/RE_6832413.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int> a(n,0),b(n,0);
+    map<int,int> mp;
+    priority_queue <pair<int,int>> p;
+    
+    priority_queue<int>pq;
+    rep(i,n){
+        cin>>a[i]>>b[i];
+        if (a[i]<=m){
+        p.push({-a[i],b[i]});
+        }
+    }
+
+    ll ans=0;
+    for (int i=1;i<=m;i++){
+          while ((-p.top().first<=i)&& (!p.empty())){
+            pq.push(p.top().second);
+            p.pop();
+        }
+        ans+=pq.top();
+        pq.pop();
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
