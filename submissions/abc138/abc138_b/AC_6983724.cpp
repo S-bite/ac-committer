9bb7c39a4c22e76883ef8a82742ac2204c1b9311
S-bite <0125bitbite@gmail.com>
2019-08-18 21:03:09 +0900

    "[AC] abc138_b"

---
+++ submissions/abc138/abc138_b/AC_6983724.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    double a[n];
+    rep(i, n) cin >> a[i];
+    double ans = 0;
+    rep(i, n) ans += 1.0 / a[i];
+    ans = 1.0 / ans;
+    printf("%f\n", ans);
+    return 0;
+}
