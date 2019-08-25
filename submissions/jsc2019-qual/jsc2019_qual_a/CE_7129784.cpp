e3a2c482496616b53be2ec993fc898af3975b973
S-bite <0125bitbite@gmail.com>
2019-08-25 12:12:08 +0900

    "[CE] jsc2019_qual_a"

---
+++ submissions/jsc2019-qual/jsc2019_qual_a/CE_7129784.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+ll MOD = 1000000007ll;
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<int> a(n, 0);
+    rep(i, n) cin >> a[i];
+    map<int, ll> mp, mp2;
+    rep(i, n) rep(j, i + 1, n)
+    {
+        if (a[i] > a[j])
+        {
+            mp[i]++;
+        }
+        if (a[j] > a[i])
+            mp2[j]++;
+    }
+    ll ans = 0;
+    ll kk = k * (k + 1) / 2;
+    kk %= MOD;
+    rep(i, n)
+    {
+        // cerr << mp[i] << endl;
+        ans += mp[i] * kk;
+        ans %= MOD;
+        ans += mp2[i] *( (k - 1) * k / 2)%MOD);
+        ans %= MOD;
+    }
+    cout << ans << endl;
+    return 0;
+}
