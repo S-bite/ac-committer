9077cc465eb6c6c4562c208c5fda4c9ef18037a5
S-bite <0125bitbite@gmail.com>
2019-09-09 19:39:50 +0900

    "[CE] cf_2015_relay_c"

---
+++ submissions/code-festival-2015-relay/cf_2015_relay_c/CE_7436456.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+  string pi="3141592653589793238462643383279502884";
+  int n;
+  cin>>n;
+  for (int i=0;i<s.size();i++){
+  	if  (s[i]-'0'==n){
+    	cout<<i<<endl;
+      	return 0;
+    }
+  }
+}
