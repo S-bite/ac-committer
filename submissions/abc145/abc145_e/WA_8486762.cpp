c3593df9e7e0b539c7b6e67604905fa8e0c5db5c
S-bite <0125bitbite@gmail.com>
2019-11-16 22:34:59 +0900

    "[WA] abc145_e"

---
+++ submissions/abc145/abc145_e/WA_8486762.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n, t;
+    cin >> n >> t;
+    vector<int> a(n), b(n);
+    map<pair<int, int>, int> mp;
+
+    rep(i, n)
+    {
+        cin >> a[i] >> b[i];
+        mp[{a[i], b[i]}]++;
+    }
+    vector<vector<int>> dp(n + 1, vector<int>(t, 0));
+    rep(i, n)
+    {
+        rep(j, t)
+        {
+            //cerr << "change " << dp[i + 1][j] << "->" << dp[i][j] << endl;
+            dp[i + 1][j] = dp[i][j];
+        }
+        rep(j, t)
+        {
+            if (a[i] + j < t)
+            {
+                if (dp[i + 1][j + a[i]] < dp[i][j] + b[i])
+                {
+                    dp[i + 1][j + a[i]] = dp[i][j] + b[i];
+                }
+            }
+        }
+    }
+    int ans = 0;
+    rep(j, t)
+    {
+        //cerr << dp[n][j] << endl;
+        rep(i, n)
+        {
+            //cerr << mp[{n, j}][i] << " ";
+            int tt = 0;
+            if (j - a[i] < 0 || dp[n][j] > dp[n][j - a[i]] + b[i] || mp[{a[i], b[i]}] > 1)
+            {
+                //  cerr << j << " " << i << " " << a[i] << " " << b[i] << endl;
+                // cerr << dp[n][j] << " " << dp[n][j - a[i]] + b[i] << endl;
+                tt = b[i];
+            }
+            ans = max(ans, dp[n][j] + tt);
+        }
+        //cerr << endl;
+    }
+    cout << ans << endl;
+    return 0;
+}
