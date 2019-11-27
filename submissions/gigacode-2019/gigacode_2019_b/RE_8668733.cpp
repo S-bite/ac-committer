d7895195032e526eb9f4f110c922f17d4f8ba467
S-bite <0125bitbite@gmail.com>
2019-11-27 12:20:32 +0900

    "[RE] gigacode_2019_b"

---
+++ submissions/gigacode-2019/gigacode_2019_b/RE_8668733.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int n,x,y,z,i,a[100],b[100],ans=0;
+  	scanf("%d%d%d%d",&n,&x,&y,&z);
+  	for (i=0;i<n;i++){
+    	scanf("%d%d",a[i],b[i]);
+    }
+  	for (i=0;i<n;i++){
+    	if ((a[i]>=x && b[i]>=y)||(a[i]+b[i]>=z)){
+        	ans++;
+        }
+    }
+  	printf("%d\n",ans);
+	return 0;
+}
