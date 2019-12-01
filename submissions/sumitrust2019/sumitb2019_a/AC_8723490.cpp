40d0b5251b9a460d4b16f4ff413db2721eb048ca
S-bite <0125bitbite@gmail.com>
2019-12-01 21:01:17 +0900

    "[AC] sumitb2019_a"

---
+++ submissions/sumitrust2019/sumitb2019_a/AC_8723490.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int m1, m2, d1, d2;
+    cin >> m1 >> d1;
+    cin >> m2 >> d2;
+    if (m1 == m2)
+        cout << 0 << endl;
+    else
+        cout << 1 << endl;
+
+    return 0;
+}
