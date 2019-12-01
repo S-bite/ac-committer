3224b8d0a4fbe78e40d1693be60e0e63d322127c
S-bite <0125bitbite@gmail.com>
2019-12-01 21:05:14 +0900

    "[AC] sumitb2019_b"

---
+++ submissions/sumitrust2019/sumitb2019_b/AC_8726712.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    rep(i, 50000)
+    {
+        if (floor((double)i * 1.08) == n)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << ":(" << endl;
+    return 0;
+}
