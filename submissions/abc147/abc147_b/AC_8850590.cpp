bc02ae6c78083c4ed8b0d826788ef23652c153d9
S-bite <0125bitbite@gmail.com>
2019-12-08 21:05:07 +0900

    "[AC] abc147_b"

---
+++ submissions/abc147/abc147_b/AC_8850590.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    int ans = 0;
+    rep(i, n / 2)
+    {
+        if (s[i] != s[n - 1 - i])
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
