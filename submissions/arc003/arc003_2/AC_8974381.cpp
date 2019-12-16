3db3ea4a4a6e6d62fdddfe7df283f2c1abf5b89e
S-bite <0125bitbite@gmail.com>
2019-12-16 11:02:20 +0900

    "[AC] arc003_2"

---
+++ submissions/arc003/arc003_2/AC_8974381.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    vector<string> v(n);
+    rep(i, n)
+    {
+        cin >> v[i];
+        reverse(ALL(v[i]));
+    }
+    sort(ALL(v));
+    rep(i, n)
+    {
+        reverse(ALL(v[i]));
+        cout << v[i] << endl;
+    }
+    return 0;
+}
