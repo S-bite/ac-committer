9ca99a303e8adfab5180a81cdb2d74d358287cde
S-bite <0125bitbite@gmail.com>
2020-01-22 12:54:18 +0900

    "[AC] agc015_a"

---
+++ submissions/agc015/agc015_a/AC_9668950.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ll n, a, b;
+    cin >> n >> a >> b;
+    cout << max((((n - 1) * b + a) - ((n - 1) * a + b)) + 1, 0ll) << endl;
+    return 0;
+}
