932b79e52043a9310186722a75502d14b9b70afe
S-bite <0125bitbite@gmail.com>
2020-01-26 21:08:08 +0900

    "[AC] abc153_d"

---
+++ submissions/abc153/abc153_d/AC_9740164.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ll h;
+    cin >> h;
+    ll ans = 0;
+    ll cur = 1;
+    while (h != 0)
+    {
+        ans += cur;
+        h = h / 2;
+        cur *= 2;
+    }
+    cout << ans << endl;
+    return 0;
+}
