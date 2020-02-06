d7fbbdee6173edb859c2937143b9ab1de292a9e1
S-bite <0125bitbite@gmail.com>
2020-02-06 20:34:20 +0900

    "[WA] abc008_2"

---
+++ submissions/abc008/abc008_2/WA_9926740.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+  	map<string,int> mp;
+  	for(int i=0;i<n;i++){
+    	string t;
+      	cin>>t;
+      	mp[t]++;
+    }
+  	string ans;
+  	int mnum=-1; 
+  	for (auto x:mp){
+    	if(x.second>mnum){
+        	mnum=x.second;
+          	ans=x.first;
+        }
+    }
+  	cout<<ans<<endl;
+  	return 0;
+}
