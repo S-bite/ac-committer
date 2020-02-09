6631539146b7317e87631242dd7cf7191bc93bae
S-bite <0125bitbite@gmail.com>
2020-02-09 21:03:37 +0900

    "[AC] abc154_c"

---
+++ submissions/abc154/abc154_c/AC_9975341.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#ifdef DEBUG
+#define line() cerr << "[" << __LINE__ << "] ";
+#define dump(i) cerr << #i ": " << i << " ";
+#define dumpl(i) cerr << #i ": " << i << endl;
+#else
+#define line(i)
+#define dump(i)
+#define dumpl(i)
+#endif
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    map<int, bool> mp;
+    rep(i, n)
+    {
+        int t;
+        cin >> t;
+        if (mp[t] == true)
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+        mp[t] = true;
+    }
+    cout << "YES" << endl;
+    return 0;
+}
