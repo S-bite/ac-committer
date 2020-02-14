36ce34ffb64b782c713319ae6f20034ce44fa312
S-bite <0125bitbite@gmail.com>
2020-02-14 14:53:51 +0900

    "[AC] s8pc_3_a"

---
+++ submissions/s8pc-3/s8pc_3_a/AC_10084807.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n, k;
+    cin >> n >> k;
+    ll ans = 0;
+    rep(i, 5)
+    {
+
+        ll cur = 81 + 9 * i;
+        int j = 0;
+        while ((cur + 63 * j) % 11 != k)
+        {
+            j++;
+        }
+        ans += (n - 2 - j + 10) / 11;
+    }
+    cout << ans << endl;
+    return 0;
+}
