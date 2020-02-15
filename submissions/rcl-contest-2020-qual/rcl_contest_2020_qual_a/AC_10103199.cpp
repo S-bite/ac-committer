c3df7d79703edb2dbdcf55af159a870a48a25c8a
S-bite <0125bitbite@gmail.com>
2020-02-15 19:05:29 +0900

    "[AC] rcl_contest_2020_qual_a"

---
+++ submissions/rcl-contest-2020-qual/rcl_contest_2020_qual_a/AC_10103199.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+class Tactics
+{
+public:
+    int score;
+    Tactics()
+    {
+    }
+    ~Tactics() {}
+};
+bool operator>(const Tactics &t1, const Tactics &t2)
+{
+    return t1.score > t2.score;
+};
+int main(int argc, char const *argv[])
+{
+    int n, w, k, v;
+    cin >> n >> w >> k >> v;
+    vector<pair<int, int>> monomino(n);
+    rep(i, n) cin >> monomino[i].second >> monomino[i].first;
+    int currrent_color = monomino[0].second;
+    rep(i, n)
+    {
+        cout << i % w << endl;
+    }
+    return 0;
+}
