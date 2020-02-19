2195fb5ae1383120196ccb2048a61b16326622da
S-bite <0125bitbite@gmail.com>
2020-02-19 10:44:24 +0900

    "[WA] abc011_3"

---
+++ submissions/abc011/abc011_3/WA_10205694.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, a, b, c;
+    cin >> n >> a >> b >> c;
+    vector<bool> dp(n + 1, false);
+    dp[n] = true;
+    rep(i, 3000)
+    {
+        auto next = dp;
+        rep(j, n + 1)
+        {
+            rep(k, 1, 4)
+            {
+                if (j - k >= 0 && j - k != a && j - k != b && j - k != c && j != a && j != b && j != c)
+                {
+                    next[j - k] = dp[j];
+                }
+            }
+        }
+        dp = next;
+    }
+    if (dp[0])
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
