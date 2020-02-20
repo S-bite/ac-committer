4bd9438c6c4cb28b9817c9a219efaf50cf044ce5
S-bite <0125bitbite@gmail.com>
2020-02-20 10:59:12 +0900

    "[AC] codefestival_2016_final_c"

---
+++ submissions/cf16-final/codefestival_2016_final_c/AC_10221179.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, m;
+    cin >> n >> m;
+    map<int, vector<int>> g;
+    rep(i, n)
+    {
+        int k;
+        cin >> k;
+        rep(j, k)
+        {
+            int l;
+            cin >> l;
+            l--;
+            g[l].push_back(m + i);
+            g[m + i].push_back(l);
+        }
+    }
+    vector<bool> visited(n + m, false);
+    deque<int> que;
+    que.push_back(m);
+    while (!que.empty())
+    {
+        int cur = que.front();
+        que.pop_front();
+        if (visited[cur])
+            continue;
+        visited[cur] = true;
+        for (auto x : g[cur])
+        {
+            if (visited[x] == false)
+            {
+                que.push_front(x);
+            }
+        }
+    }
+    bool ok = true;
+    rep(i, m, n + m)
+    {
+        ok &= visited[i];
+    }
+    if (ok)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
