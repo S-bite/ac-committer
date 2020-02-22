8b591ff478ecc56da65e465f98f728920762db42
S-bite <0125bitbite@gmail.com>
2020-02-22 21:05:35 +0900

    "[AC] abc156_a"

---
+++ submissions/abc156/abc156_a/AC_10269768.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1e9 + 7;
+
+int main(int argc, char const *argv[])
+{
+    int n, r;
+    cin >> n >> r;
+    if (n >= 10)
+    {
+        cout << r << endl;
+    }
+    else
+    {
+        cout << r + 100 * (10 - n) << endl;
+    }
+    return 0;
+}
