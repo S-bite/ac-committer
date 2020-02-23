5ec7c20d6dd41cf63604f3d15eecb6a48343de64
S-bite <0125bitbite@gmail.com>
2020-02-23 23:19:41 +0900

    "[AC] arc089_a"

---
+++ submissions/abc086/arc089_a/AC_10313907.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1e9 + 7;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    vector<int> t(n), x(n), y(n);
+    rep(i, n) cin >> t[i] >> x[i] >> y[i];
+    rep(i, n)
+    {
+        if (x[i] + y[i] > t[i] || (x[i] + y[i]) % 2 != t[i] % 2)
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
