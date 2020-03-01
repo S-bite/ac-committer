7525926e90fb4e334aa0f5a02863ffd152cfc23a
S-bite <0125bitbite@gmail.com>
2020-03-02 04:02:30 +0900

    "[WA] abc009_3"

---
+++ submissions/abc009/abc009_3/WA_10477516.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+const int IINF = 1 << 29;
+typedef vector<vector<int>> VVI;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+int main(int argc, char const *argv[])
+{
+    int n, k;
+    cin >> n >> k;
+    int tmp = k;
+    string s;
+    cin >> s;
+    auto t = s;
+    auto old = s;
+    VI cost(n, 1);
+    sort(ALL(t));
+    rep(i, n)
+    {
+        if (s[i] == t[i])
+            continue;
+        for (int j = n - 1; j > i; j--)
+        {
+            if (s[j] == t[i])
+            {
+                if (k >= cost[i] + cost[j])
+                {
+                    swap(s[i], s[j]);
+                    k -= cost[i] + cost[j];
+                    break;
+                }
+            }
+        }
+        //cerr << s << endl;
+        rep(j, n)
+        {
+            cost[j] = (s[j] == old[j] ? 1 : 0);
+            //  cerr << cost[j];
+        }
+        //cerr << endl;
+        //cerr << k << endl;
+    }
+    rep(i, n)
+    {
+        if (s[i] != old[i])
+            tmp--;
+    }
+    assert(tmp >= 0);
+    cout << s << endl;
+
+    return 0;
+}
