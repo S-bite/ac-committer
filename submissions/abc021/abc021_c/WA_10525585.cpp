941b0485d282aa989e0b5b58ab0f1a86da2d50ad
S-bite <0125bitbite@gmail.com>
2020-03-04 18:38:55 +0900

    "[WA] abc021_c"

---
+++ submissions/abc021/abc021_c/WA_10525585.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#define int ll
+using namespace std;
+const int IINF = 1 << 29;
+const int MOD = 1e9 + 7;
+signed main(int argc, char const *argv[])
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    a--;
+    b--;
+    int m;
+    cin >> m;
+    map<int, vector<int>> g;
+    rep(i, m)
+    {
+        int x, y;
+        cin >> x >> y;
+        x--;
+        y--;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    vector<int> num(n, 0);
+    vector<bool> visited(n, false);
+    deque<int> que;
+    que.push_back(a);
+    num[a] = 1;
+    vector<bool> inque(n, false);
+    inque[a] = true;
+
+    while (!que.empty())
+    {
+        deque<int> nque;
+        while (!que.empty())
+        {
+            int cur = que.front();
+            que.pop_front();
+            if (visited[cur])
+                continue;
+            visited[cur] = true;
+            for (auto next : g[cur])
+            {
+                if (inque[next] == false)
+                {
+                    inque[next] = true;
+                    nque.push_front(next);
+                }
+                if (visited[next] == false)
+                {
+
+                    num[next] += num[cur];
+                    num[next] %= MOD;
+                }
+            }
+        }
+        que = nque;
+    }
+    cout << num[b] << endl;
+    return 0;
+}
