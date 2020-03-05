9694f684ba3dbc0a69e59437a57d5872c41c6630
S-bite <0125bitbite@gmail.com>
2020-03-06 02:51:31 +0900

    "[WA] arc064_a"

---
+++ submissions/abc048/arc064_a/WA_10552983.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    rep(i, n) cin >> v[i];
+    auto tmp = v;
+    int ans1 = 0, ans2 = 0;
+    rep(i, n)
+    {
+        ans1 += max(0, tmp[i] + tmp[i + 1] - k);
+        tmp[i] -= max(0, tmp[i] + tmp[i + 1] - k);
+        i++;
+    }
+    tmp = v;
+    rep(i, 1, n)
+    {
+        ans2 += max(0, tmp[i] + tmp[i - 1] - k);
+        tmp[i] -= max(0, tmp[i] + tmp[i - 1] - k);
+        i++;
+    }
+    tmp = v;
+    cout << min(ans1, ans2) << endl;
+
+    return 0;
+}
