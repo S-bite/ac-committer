2bed3d5f5eaf7189e4032e886bbdc3509cf850f4
S-bite <0125bitbite@gmail.com>
2020-03-06 02:47:30 +0900

    "[AC] arc063_a"

---
+++ submissions/abc047/arc063_a/AC_10552958.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string s;
+    cin >> s;
+    int ans = 0;
+    char cur = '@';
+    rep(i, s.size())
+    {
+        if (cur != s[i])
+        {
+            ans++;
+            cur = s[i];
+        }
+    }
+    cout << ans - 1 << endl;
+    return 0;
+}
