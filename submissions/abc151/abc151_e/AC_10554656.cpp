71c6352403812944ed07a38654430fe06878f5a7
S-bite <0125bitbite@gmail.com>
2020-03-06 08:11:17 +0900

    "[AC] abc151_e"

---
+++ submissions/abc151/abc151_e/AC_10554656.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit()
+{
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++)
+    {
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k)
+{
+    if (n < k)
+        return 0;
+    if (n < 0 || k < 0)
+        return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main(int argc, char const *argv[])
+{
+    COMinit();
+    int n, k;
+    cin >> n >> k;
+    vector<ll> v(n);
+    rep(i, n) cin >> v[i];
+    sort(ALL(v));
+    ll ans = 0;
+    rep(i, n)
+    {
+        ans -= v[i] * COM(n - 1 - i, k - 1);
+        ans += v[i] * COM(i, k - 1);
+        ans %= MOD;
+        while (ans < 0)
+            ans += MOD;
+    }
+    rep(i, n)
+    {
+    }
+    cout << ans << endl;
+
+    return 0;
+}
