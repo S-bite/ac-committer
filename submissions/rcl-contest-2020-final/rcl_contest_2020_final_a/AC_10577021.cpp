b9341ff9cf15ffc4d7766240c74163623e7e7081
S-bite <0125bitbite@gmail.com>
2020-03-07 14:06:19 +0900

    "[AC] rcl_contest_2020_final_a"

---
+++ submissions/rcl-contest-2020-final/rcl_contest_2020_final_a/AC_10577021.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+#define chmin(x, y) x = min(x, y);
+using namespace std;
+
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1e9 + 7;
+
+int n, m;
+vector<int> d;
+void print_dice(vector<int> &d)
+{
+    rep(i, 6) cout << d[i] << " ";
+    cout << endl;
+}
+int main(int argc, char const *argv[])
+{
+    d.resize(6);
+    rep(i, 6)
+    {
+        d[i] = i + 1;
+    }
+    cin >> n >> m;
+    int pos = 0;
+    int score = 0;
+    rep(i, m)
+    {
+        int res, v;
+        print_dice(d);
+        cin >> res >> v >> pos;
+        score += v;
+        if (pos == n)
+        {
+            pos = 0;
+        }
+    }
+    return 0;
+}
