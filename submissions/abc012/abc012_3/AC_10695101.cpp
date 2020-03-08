7abd0baa622630a3d34190a523d7f71792d2d559
S-bite <0125bitbite@gmail.com>
2020-03-09 01:37:53 +0900

    "[AC] abc012_3"

---
+++ submissions/abc012/abc012_3/AC_10695101.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int total = 45 * 45;
+    int n;
+    cin >> n;
+    n = total - n;
+    rep(i, 1, 10)
+    {
+        if (n % i == 0 && n / i <= 9)
+        {
+            cout << i << " x " << n / i << endl;
+        }
+    }
+    return 0;
+}
