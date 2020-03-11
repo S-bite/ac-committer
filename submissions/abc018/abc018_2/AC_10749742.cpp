ea95e3358f7851f8010b97378cc414c69233118a
S-bite <0125bitbite@gmail.com>
2020-03-11 13:38:39 +0900

    "[AC] abc018_2"

---
+++ submissions/abc018/abc018_2/AC_10749742.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int n;
+    cin >> n;
+    rep(i, n)
+    {
+        int l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+        reverse(s.begin() + l, s.begin() + r + 1);
+    }
+    cout << s << endl;
+    return 0;
+}
