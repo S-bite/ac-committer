d752796e8db5d3db468ac2bdda1f62764ffd8e70
S-bite <0125bitbite@gmail.com>
2020-03-11 14:02:16 +0900

    "[AC] abc005_2"

---
+++ submissions/abc005/abc005_2/AC_10750101.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    rep(i, n) { cin >> v[i]; }
+    sort(ALL(v));
+    cout << v[0] << endl;
+    return 0;
+}
