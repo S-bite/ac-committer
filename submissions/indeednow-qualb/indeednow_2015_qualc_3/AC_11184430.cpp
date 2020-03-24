d1cb822d15a8f79b337b96a5f0c672d6a8b38ae5
S-bite <0125bitbite@gmail.com>
2020-03-25 00:20:13 +0900

    "[AC] indeednow_2015_qualc_3"

---
+++ submissions/indeednow-qualb/indeednow_2015_qualc_3/AC_11184430.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    map<int, vector<int>> g;
+    rep(i, n - 1)
+    {
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    map<int, bool> visited;
+    priority_queue<int, vector<int>, greater<int>> pque;
+    pque.push(1);
+    vector<int> ans;
+    while (!pque.empty())
+    {
+        int cur = pque.top();
+        pque.pop();
+        if (visited[cur] == true)
+        {
+            continue;
+        }
+        ans.push_back(cur);
+        visited[cur] = true;
+        for (auto x : g[cur])
+        {
+            if (visited[x] == false)
+            {
+                pque.push(x);
+            }
+        }
+    }
+    rep(i, n)
+    {
+        cout << ans[i] << (i == n - 1 ? "" : " ");
+    }
+    cout << endl;
+    return 0;
+}
