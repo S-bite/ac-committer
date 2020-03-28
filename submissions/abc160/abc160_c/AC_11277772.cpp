cc2bebd63524274eed9863f72b52db3ab687d73a
S-bite <0125bitbite@gmail.com>
2020-03-28 21:07:15 +0900

    "[AC] abc160_c"

---
+++ submissions/abc160/abc160_c/AC_11277772.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+int main()
+{
+    ll k, n;
+    cin >> k >> n;
+    vector<int> v(n);
+    rep(i, n) cin >> v[i];
+    ll ans = v[n - 1] - v[0];
+    rep(i, n - 1)
+    {
+        ans = min(ans, k - (v[i + 1] - v[i]));
+    }
+    cout << ans << endl;
+    return 0;
+}
