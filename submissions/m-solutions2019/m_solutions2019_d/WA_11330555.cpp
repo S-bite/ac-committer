47091c754c9dcad16dd9e45c362f21bd9c20584f
S-bite <0125bitbite@gmail.com>
2020-03-29 11:25:30 +0900

    "[WA] m_solutions2019_d"

---
+++ submissions/m-solutions2019/m_solutions2019_d/WA_11330555.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+map<int, vector<int>> g;
+
+int main()
+{
+    int n;
+    cin >> n;
+    rep(i, n - 1)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    vector<int> c(n);
+    rep(i, n) cin >> c[i];
+    sort(ALL(c));
+    vector<pair<int, int>> deg(n);
+    rep(i, n)
+    {
+        deg[i].first = g[i].size();
+        deg[i].second = i;
+    }
+    sort(ALL(deg));
+    map<int, int> mp;
+    rep(i, n)
+    {
+        mp[deg[i].second] = c[i];
+    }
+    ll ans = 0;
+    rep(i, n)
+    {
+        for (auto x : g[i])
+        {
+            if (x > i)
+            {
+                ans += min(mp[i], mp[x]);
+            }
+        }
+    }
+    cout << ans << endl;
+    rep(i, n) cout << mp[i] << " ";
+    cout << endl;
+
+    return 0;
+}
