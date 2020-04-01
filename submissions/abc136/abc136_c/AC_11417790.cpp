c42dbb4cc9bf79ce7d4d64c8644bea4bc837c10a
S-bite <0125bitbite@gmail.com>
2020-04-01 21:53:28 +0900

    "[AC] abc136_c"

---
+++ submissions/abc136/abc136_c/AC_11417790.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+
+int main()
+{
+    int n;
+    cin >> n;
+    VI h(n);
+    rep(i, n)
+    {
+        cin >> h[i];
+    }
+    reverse(ALL(h));
+    rep(i, 1, n)
+    {
+        if (h[i - 1] < h[i])
+        {
+            h[i]--;
+        }
+        if (h[i - 1] < h[i])
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
