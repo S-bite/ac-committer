e0812265b3f0f02eee8af806fde989805d44db88
S-bite <0125bitbite@gmail.com>
2020-04-01 22:12:52 +0900

    "[AC] abc151_e"

---
+++ submissions/abc151/abc151_e/AC_11418392.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit()
+{
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++)
+    {
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k)
+{
+    if (n < k)
+        return 0;
+    if (n < 0 || k < 0)
+        return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main()
+{
+    COMinit();
+    int n, k;
+    cin >> n >> k;
+    VI v(n);
+    rep(i, n)
+    {
+        cin >> v[i];
+    }
+    sort(ALL(v));
+    ll ans = 0;
+    rep(i, n)
+    {
+        ans += v[i] * COM(i, k - 1);
+        ans -= v[i] * COM(n - 1 - i, k - 1);
+        ans %= MOD;
+    }
+    cout << ans << endl;
+    return 0;
+}
