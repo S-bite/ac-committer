00748a4b1f2185cdf12ae1b0ae7d21c708b1f2b9
S-bite <0125bitbite@gmail.com>
2020-04-05 21:10:01 +0900

    "[AC] judge_update_202004_d"

---
+++ submissions/judge-update-202004/judge_update_202004_d/AC_11584167.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+random_device rnd;
+mt19937 mt(rnd());
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1000000007;
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    VI v(n);
+    VI acc(n);
+    rep(i, n) cin >> v[i];
+    acc[0] = v[0];
+    rep(i, n - 1)
+    {
+        acc[i + 1] = gcd(acc[i], v[i + 1]);
+    }
+    rep(i, q)
+    {
+        int t;
+        cin >> t;
+        if (gcd(t, acc[n - 1]) != 1)
+        {
+            cout << gcd(t, acc[n - 1]) << endl;
+        }
+        else
+        {
+            int r = n;
+            int l = -1;
+            while (l + 1 != r)
+            {
+                int med = (l + r) / 2;
+                if (gcd(acc[med], t) == 1)
+                {
+                    r = med;
+                }
+                else
+                {
+                    l = med;
+                }
+            }
+            cout << r + 1 << endl;
+        }
+    }
+    return 0;
+}
