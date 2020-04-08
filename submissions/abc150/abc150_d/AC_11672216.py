ef4b8509730349390524ed76a6df5d18cabda0e4
S-bite <0125bitbite@gmail.com>
2020-04-08 22:18:32 +0900

    "[AC] abc150_d"

---
+++ submissions/abc150/abc150_d/AC_11672216.py
@@ -0,0 +1,30 @@
+import fractions
+
+
+def lcm(x, y):
+    return (x * y) // fractions.gcd(x, y)
+
+
+n, m = map(int, input().split())
+v = list(map(int, input().split()))
+num = 0
+# print(v[0])
+while (v[0] % 2 == 0):
+    v[0] //= 2
+    num += 1
+# print(v[0])
+ans = v[0]
+for i in range(n - 1):
+    if (v[i + 1] % (2 ** num) != 0 or v[i + 1] % (2 ** (num + 1)) == 0):
+        ans = 2
+        break
+    # print(v[i+1])
+    v[i+1] //= 2**num
+    # print(v[i+1])
+    ans = lcm(ans, v[i + 1])
+# print(ans)
+if (ans % 2 == 0):
+    print(0)
+    exit(0)
+ans *= 2**(num-1)
+print(((m//ans)+1)//2)
