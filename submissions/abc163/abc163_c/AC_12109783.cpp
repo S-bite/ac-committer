ca6804e2ecb89318e3c7fdf83b12df2dff72cb7e
S-bite <0125bitbite@gmail.com>
2020-04-19 21:13:40 +0900

    "[AC] abc163_c"

---
+++ submissions/abc163/abc163_c/AC_12109783.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+random_device rnd;
+mt19937 mt(rnd());
+
+double PI = 6.28318530717958623200 / 2.0;
+
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1e9 + 7;
+int main()
+{
+    int n;
+    cin >> n;
+    map<int, int> mp;
+    rep(i, 1, n)
+    {
+        int t;
+        cin >> t;
+        t--;
+        mp[t]++;
+    }
+    rep(i, n)
+    {
+        cout << mp[i] << endl;
+    }
+    return 0;
+}
