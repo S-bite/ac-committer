c5974983a2900de4e0a1fc149e8dddfd511ee185
S-bite <0125bitbite@gmail.com>
2020-04-26 21:03:32 +0900

    "[AC] abc164_b"

---
+++ submissions/abc164/abc164_b/AC_12341624.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+random_device rnd;
+mt19937 mt(rnd());
+
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1e9 + 7;
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    while (true)
+    {
+        c -= b;
+        if (c <= 0)
+        {
+            cout << "Yes" << endl;
+            return 0;
+        }
+        a -= d;
+        if (a <= 0)
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
