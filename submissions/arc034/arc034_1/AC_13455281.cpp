d2f31cc34336fc459f768e1fd80612da92baaa7a
S-bite <0125bitbite@gmail.com>
2020-05-21 21:07:51 +0900

    "[AC] arc034_1"

---
+++ submissions/arc034/arc034_1/AC_13455281.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define chmax(a,b) a=max(a,b)
+using namespace std;
+int main(){
+	int n;
+ 	cin>>n;
+  	long double ans=-1;
+  	for(int i=0;i<n;i++){
+    	long double cur=0;
+      	long double t;
+    	for (int j=0;j<4;j++){
+	    	cin>>t;
+          	cur+=t;
+        }
+      	cin>>t;
+      	cur+=t*110/900.0;
+      	chmax(ans,cur);
+    }
+  	cout<<fixed<<setprecision(18)<<ans<<endl;
+}
