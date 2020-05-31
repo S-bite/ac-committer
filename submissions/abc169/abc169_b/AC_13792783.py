7d8b6f9add08ec04fac0a1d08a2897cf5694a3c9
S-bite <0125bitbite@gmail.com>
2020-05-31 21:07:23 +0900

    "[AC] abc169_b"

---
+++ submissions/abc169/abc169_b/AC_13792783.py
@@ -0,0 +1,12 @@
+n = int(input())
+v = list(map(int, input().split()))
+if 0 in v:
+    print(0)
+    exit()
+ans = 1
+for i in range(n):
+    ans *= v[i]
+    if (ans > 10**18):
+        print(-1)
+        exit()
+print(ans)
