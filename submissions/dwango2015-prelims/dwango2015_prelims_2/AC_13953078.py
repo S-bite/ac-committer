08ead5caffdc87f009e012734bffcd3a614f9878
S-bite <0125bitbite@gmail.com>
2020-06-02 20:39:03 +0900

    "[AC] dwango2015_prelims_2"

---
+++ submissions/dwango2015-prelims/dwango2015_prelims_2/AC_13953078.py
@@ -0,0 +1,17 @@
+s=input()
+if (len(s)%2==1):
+  s+="@"
+ans=0
+cur=0
+i=0
+while(i<len(s)):
+  if (i+1<len(s) and s[i]=="2" and s[i+1]=="5"):
+    i+=2
+    cur+=1
+    continue
+  else:
+    ans+=((cur+1)*cur)//2
+    cur=0
+  i+=1
+ans+=((cur+1)*cur)//2
+print(ans)
