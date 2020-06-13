d2ef7238848b8d8a97359d843b4a91d1b5c918fb
S-bite <0125bitbite@gmail.com>
2020-06-13 21:03:38 +0900

    "[AC] tokiomarine2020_b"

---
+++ submissions/tokiomarine2020/tokiomarine2020_b/AC_14222901.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+random_device rnd;
+mt19937 mt(rnd());
+
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1e9 + 7;
+
+int main()
+{
+    ll a, v, b, w, t;
+    cin >> a >> v >> b >> w >> t;
+    if (a == b)
+    {
+        cout << "YES" << endl;
+        return 0;
+    }
+    if (w >= v)
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+    if ((v - w) * t >= llabs(a - b))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
