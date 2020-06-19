bc1365825f89a9effbfee5222d87ccf26528ef2c
S-bite <0125bitbite@gmail.com>
2020-06-19 21:51:44 +0900

    "[CE] abc170_b"

---
+++ submissions/abc170/abc170_b/CE_14473642.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a,b;
+  	cin>>a>>b;
+  	for(int i=0;i<=a;i++){
+    	if (2*i+4*(a-i)==b){
+        	cout<<"Yes"<<endl;
+          	return 0:
+        }
+    }
+  cout<<"No"<<endl;
+  return 0;
+}
