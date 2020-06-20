2ef131e73abfd83d1f7f30c0b4d8457e083ecfe1
S-bite <0125bitbite@gmail.com>
2020-06-20 20:50:55 +0900

    "[AC] past202005_a"

---
+++ submissions/past202005-open/past202005_a/AC_14494229.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+random_device rnd;
+mt19937 mt(rnd());
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1000000007;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    if (s == t)
+    {
+        cout << "same" << endl;
+    }
+    else
+    {
+        transform(ALL(s), s.begin(), ::tolower);
+        transform(ALL(t), t.begin(), ::tolower);
+        if (s == t)
+        {
+            cout << "case-insensitive" << endl;
+        }
+        else
+        {
+            cout << "different" << endl;
+        }
+    }
+    return 0;
+}
