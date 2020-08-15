5c111160c28f0c93e4ffc8978a14d49c6298e35f
S-bite <example.com>
2020-08-15 22:47:27 +0900

    [WA] abc175_b

---
+++ submissions/abc175/abc175_b/WA_15958961.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+ans=0
+for a in l:
+  for b in l:
+    for c in l:
+      if (a==b or b==c or a==c):
+        continue
+      if max([a,b,c])<a+b+c-max([a,b,c]):
+        ans+=1
+print(ans)
