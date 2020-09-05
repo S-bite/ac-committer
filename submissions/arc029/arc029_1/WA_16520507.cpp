70d181d8a6ffc068975280f4cde2569fe5f5141a
S-bite <example.com>
2020-09-05 20:12:49 +0900

    [WA] arc029_1

---
+++ submissions/arc029/arc029_1/WA_16520507.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+  int n;
+  cin>>n;
+  int a[n];
+  int sum=0;
+  for(int i=0;i<n;i++){
+  	cin>>a[i];
+    sum+=a[i];
+  }
+  int ans=1000000;
+  for (int i=0;i<1<<n;i++){
+    int x=0;
+    for (int j=0;j<n;j++){
+      if (((j>>i)&1)==1){
+        x+=a[j];
+      }
+    }
+    ans=min(ans,max(x,sum-x));
+  }
+  cout<<ans<<endl;
+  return 0;
+}
