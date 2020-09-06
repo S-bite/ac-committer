60d0ebd22fdeae4c0ed03dc8809328798cf02c94
S-bite <example.com>
2020-09-06 21:14:14 +0900

    [WA] joi2020_yo1c_a

---
+++ submissions/joi2020yo1c/joi2020_yo1c_a/WA_16542540.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int x,l,r;
+  cin>>x>>l>>r;
+  int ans=l;
+  for (int i=l;i<=r;i++){
+    if (abs(x-ans)>abs(x-i)){
+    	ans=x;
+    }
+  }
+  cout<<ans<<endl;
+  return 0;
+}
