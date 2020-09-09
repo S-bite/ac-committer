496e5baf21f8e8a0dfea143e463514751d8b5f4e
S-bite <example.com>
2020-09-09 21:16:22 +0900

    [RE] agc024_c

---
+++ submissions/agc024/agc024_c/RE_16597221.py
@@ -0,0 +1,15 @@
+n=int(input())
+l=[int(input()) for _ in range(n)]
+ans=0
+if l[0]!=0:
+  print(-1)
+  exit()
+for i in range(n-1):
+  if l[i+1]-l[i]>1:
+    print(-1)
+    exit()
+  elif (l[i]+1==l[i+1]):
+    ans+=1
+  else:
+	ans+=l[i+1]
+print(ans)
