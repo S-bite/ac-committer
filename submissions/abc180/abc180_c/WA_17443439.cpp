aaf8d4f10c31719bde64ed354f3581cc11b6ec75
S-bite <0125bitbite@gmail.com>
2020-10-17 20:05:54 +0900

    [WA] abc180_c

---
+++ submissions/abc180/abc180_c/WA_17443439.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+random_device rnd;
+mt19937 mt(rnd());
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1000000007;
+
+int main()
+{
+    ll n;
+    cin >> n;
+    vector<ll> ans;
+    ll i = 1;
+    while (i * i < n) {
+        if (n % i == 0) {
+
+            ans.push_back(i);
+            ans.push_back(n / i);
+        }
+        i++;
+    }
+    if (n % i == 0) {
+        ans.push_back(i);
+    }
+    sort(ALL(ans));
+    for (auto&& x : ans) {
+        cout << x << endl;
+    }
+    return 0;
+}
