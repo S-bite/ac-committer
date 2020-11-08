a0b1506f573d8e52346702bd71fe42d78a602de8
S-bite <0125bitbite@gmail.com>
2020-11-09 00:26:27 +0900

    [AC] abc182_c

---
+++ submissions/abc182/abc182_c/AC_17999575.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (ll i = (ll)(a); i < (ll)(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ll long long
+#define lld long double
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+
+const double EPS = 1e-3;
+const double PI = 3.1415926535897932384626433832795028841971;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1e9 + 7;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int a = 0, b = 0;
+    for (char c : s)
+    {
+        if (((c - '0') % 3) == 1)
+            a++;
+        if (((c - '0') % 3) == 2)
+            b++;
+    }
+    if (a < b)
+        swap(a, b);
+    a %= 3, b %= 3;
+    if (a == 0 && b == 2)
+        a = 1;
+    else
+        a = abs(a - b) % 3;
+    if (s.size() <= a)
+        cout << -1 << endl;
+    else
+        cout << a << endl;
+    return 0;
+}
