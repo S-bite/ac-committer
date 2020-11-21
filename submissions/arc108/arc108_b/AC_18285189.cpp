73a5c4510e28f50f2ecc33e8e25f8ce6403380ab
S-bite <0125bitbite@gmail.com>
2020-11-21 23:40:27 +0900

    [AC] arc108_b

---
+++ submissions/arc108/arc108_b/AC_18285189.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+random_device rnd;
+mt19937 mt(rnd());
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1000000007;
+
+int main()
+{
+    char st[505050];
+    int top = 0;
+    int n;
+    cin >> n;
+    rep(i, n)
+    {
+        cin >> st[top];
+        top++;
+        if (top >= 3 && st[top - 1] == 'x' && st[top - 2] == 'o' && st[top - 3] == 'f') {
+            top -= 3;
+        }
+    }
+    while (top > 3 && st[top - 1] == 'x' && st[top - 2] == 'o' && st[top - 3] == 'f') {
+        top -= 3;
+    }
+
+    cout << top << endl;
+    return 0;
+}
