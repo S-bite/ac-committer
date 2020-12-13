692b3e0000746a4ca5e14a52e7451574c80d3647
S-bite <0125bitbite@gmail.com>
2020-12-13 22:57:04 +0900

    [AC] abc185_b

---
+++ submissions/abc185/abc185_b/AC_18765019.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+random_device rnd;
+mt19937 mt(rnd());
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1000000007;
+
+int main()
+{
+    ll n, m, t;
+    cin >> n >> m >> t;
+    ll curp = n;
+    ll curt = 0;
+    rep(i, m)
+    {
+        ll a, b;
+        cin >> a >> b;
+        curp -= a - curt;
+        if (curp <= 0)
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+        curt = a;
+        curp = min(n, curp + b - a);
+        curt = b;
+    }
+    curp -= t - curt;
+    if (curp <= 0)
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+    return 0;
+}
