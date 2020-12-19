a2438254a57c1a5a9d056bc242674130e7b3c1b9
S-bite <0125bitbite@gmail.com>
2020-12-19 21:05:21 +0900

    [AC] abc186_c

---
+++ submissions/abc186/abc186_c/AC_18866895.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+random_device rnd;
+mt19937 mt(rnd());
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1000000007;
+
+bool has7(int n)
+{
+    int t = n;
+    while (t != 0)
+    {
+        if (t % 8 == 7)
+        {
+            return false;
+        }
+        t /= 8;
+    }
+    t = n;
+    while (t != 0)
+    {
+        if (t % 10 == 7)
+        {
+            return false;
+        }
+        t /= 10;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+    rep(i, 1, n + 1)
+    {
+        if (has7(i))
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
