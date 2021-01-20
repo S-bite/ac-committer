1f17a7934ceea9fe1d69a74946c01a5064737e09
S-bite <0125bitbite@gmail.com>
2021-01-20 21:57:44 +0900

    [AC] abc064_a

---
+++ submissions/abc064/abc064_a/AC_19538174.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define contain(x, y) (x.find(y) != x.end())
+
+using namespace std;
+using namespace atcoder;
+random_device rnd;
+mt19937 mt(rnd());
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1000000007;
+int main()
+{
+    int r, g, b;
+    cin >> r >> g >> b;
+    cout << (((100 * r + 10 * g + b) % 4 == 0) ? "YES" : "NO") << endl;
+}
