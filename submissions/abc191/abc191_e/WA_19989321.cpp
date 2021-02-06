ae7959c87335c624e95cf963561d80cba29c61c7
S-bite <0125bitbite@gmail.com>
2021-02-06 22:07:08 +0900

    [WA] abc191_e

---
+++ submissions/abc191/abc191_e/WA_19989321.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+#define ALL(x) x.begin(), x.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+random_device rnd;
+mt19937 mt(rnd());
+using ll = long long;
+using lld = long double;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<ll>;
+using VVL = vector<VL>;
+using PII = pair<int, int>;
+const int IINF = 1 << 30;
+const ll INF = 1ll << 60;
+const ll MOD = 1000000007;
+
+typedef map<int, vector<int>> Graph;
+typedef map<int, map<int, ll>> WGraph;
+
+vector<ll> dijkstra(int v, WGraph &g, int r)
+{
+    vector<ll> cost(v, INF);
+    cost[r] = 0;
+    priority_queue<pair<ll, int>> pq;
+
+    pq.push({0, r});
+
+    while (!pq.empty())
+    {
+        //cerr << "-------------------------------" << endl;
+        auto t = pq.top();
+        pq.pop();
+        //cerr << t.second << "," << t.first << endl;
+        if (cost[t.second] < -t.first)
+        {
+            continue;
+        }
+        for (auto x : g[t.second])
+        {
+            if (cost[x.first] > cost[t.second] + g[t.second][x.first])
+            {
+                cost[x.first] = cost[t.second] + g[t.second][x.first];
+                //cerr << "update cost[" << x.first << "] with " << cost[x.first] << endl;
+                pq.push({-cost[x.first], x.first});
+            }
+        }
+    }
+    return cost;
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    WGraph g;
+    vector<ll> self(n, -1);
+    map<pair<int, int>, bool> used;
+    rep(i, m)
+    {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        if (used[{a, b}] == false)
+        {
+            g[a][b] = c;
+        }
+        else
+        {
+            chmin(g[a][b], c);
+        }
+        used[{a, b}] = true;
+
+        if (a == b)
+        {
+            if (self[a] == -1)
+            {
+                self[a] = c;
+            }
+            else
+            {
+                chmin(self[a], c);
+            }
+        }
+    }
+    vector<vector<ll>> cost(n);
+    rep(i, n)
+    {
+        cost[i] = dijkstra(n, g, i);
+    }
+    rep(i, n)
+    {
+        ll ans = INF;
+        rep(j, n)
+        {
+            if (i == j)
+            {
+                if (self[i] != -1)
+                {
+                    chmin(ans, self[i]);
+                }
+            }
+            else if (cost[i][j] + cost[j][i] < ans)
+            {
+                ans = cost[i][j] + cost[j][i];
+            }
+        }
+        if (ans == IINF)
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << ans << endl;
+        }
+    }
+
+    return 0;
+}
